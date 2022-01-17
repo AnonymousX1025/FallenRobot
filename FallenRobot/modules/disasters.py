f67dde18d9170d2d30e0aef28521f19903215bd3
Anonymous Boy <rahulking25102006@gmail.com>
2022-01-17 11:48:44 +0530

    Update disasters.py

--- FallenRobot/modules/disasters.py
+++ FallenRobot/modules/disasters.py
@@ -677,7 +677,7 @@ dispatcher.add_handler(SUPPORTLIST_HANDLER)
 dispatcher.add_handler(SUDOLIST_HANDLER)
 dispatcher.add_handler(DEVLIST_HANDLER)
 
-__mod_name__ = "FU*KERS"
+__mod_name__ = "ꜰᴜ*ᴋᴇʀs😘"
 __handlers__ = [
     SUDO_HANDLER,
     SUPPORT_HANDLER,
