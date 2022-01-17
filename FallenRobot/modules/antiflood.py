740e06d72c48578e018539a7b61b9cbffd7cd5f6
Anonymous Boy <rahulking25102006@gmail.com>
2022-01-17 10:25:05 +0530

    Update antiflood.py

--- FallenRobot/modules/antiflood.py
+++ FallenRobot/modules/antiflood.py
@@ -419,7 +419,7 @@ will result in restricting that user.
  `1w` = 1 week
  """
 
-__mod_name__ = "ANTI-FLOOD"
+__mod_name__ = "ᴀɴᴛɪ-ꜰʟᴏᴏᴅ🌊"
 
 FLOOD_BAN_HANDLER = MessageHandler(
     Filters.all & ~Filters.status_update & Filters.group, check_flood
