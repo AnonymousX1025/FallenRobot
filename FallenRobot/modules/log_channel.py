fbd5a971891f2be787f841682348869380379a6b
Anonymous Boy <rahulking25102006@gmail.com>
2022-01-17 12:08:48 +0530

    Update log_channel.py

--- FallenRobot/modules/log_channel.py
+++ FallenRobot/modules/log_channel.py
@@ -204,7 +204,7 @@ Setting the log channel is done by:
 ❍ forwarding the /setlog to the group
 """
 
-    __mod_name__ = "CHANNEL"
+    __mod_name__ = "ʟᴏɢs​🗒"
 
     LOG_HANDLER = CommandHandler("logchannel", logging)
     SET_LOG_HANDLER = CommandHandler("setlog", setlog)
