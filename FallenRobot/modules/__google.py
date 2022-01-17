061a743b9a80c8bc8e395086fa64402ff21d3511
Anonymous Boy <rahulking25102006@gmail.com>
2022-01-17 11:42:55 +0530

    Update __google.py

--- FallenRobot/modules/__google.py
+++ FallenRobot/modules/__google.py
@@ -275,7 +275,7 @@ async def apk(e):
         await e.reply("Exception Occured:- " + str(err))
 
 
-__mod_name__ = "GOOGLE"
+__mod_name__ = "ɢᴏᴏɢʟᴇ🌐"
 
 __help__ = """
  ❍ /google <text>*:* Perform a google search
