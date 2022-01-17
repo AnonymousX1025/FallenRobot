3f43e478c7db03f7a4200b7bf5dcac803ff84e27
Anonymous Boy <rahulking25102006@gmail.com>
2022-01-17 10:55:27 +0530

    Update warns.py

--- FallenRobot/modules/warns.py
+++ FallenRobot/modules/warns.py
@@ -522,7 +522,7 @@ be a sentence, encompass it with quotes, as such: `/addwarn "very angry" This is
  ❍ /strongwarn <on/yes/off/no>*:* If set to on, exceeding the warn limit will result in a ban. Else, will just punch.
 """
 
-__mod_name__ = "WARNS"
+__mod_name__ = "ᴡᴀʀɴs⚠️"
 
 WARN_HANDLER = CommandHandler(["warn", "dwarn"], warn_user, filters=Filters.group)
 RESET_WARN_HANDLER = CommandHandler(
