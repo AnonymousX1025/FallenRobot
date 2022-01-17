436276efe01e8890dd24e1fd6f92a332b6786bf4
Anonymous Boy <rahulking25102006@gmail.com>
2022-01-17 10:46:57 +0530

    Update welcome.py

--- FallenRobot/modules/welcome.py
+++ FallenRobot/modules/welcome.py
@@ -1144,7 +1144,7 @@ dispatcher.add_handler(CLEAN_SERVICE_HANDLER)
 dispatcher.add_handler(BUTTON_VERIFY_HANDLER)
 dispatcher.add_handler(WELCOME_MUTE_HELP)
 
-__mod_name__ = "WELCOME"
+__mod_name__ = "ᴡᴇʟᴄᴏᴍᴇ😘"
 __command_list__ = []
 __handlers__ = [
     NEW_MEM_HANDLER,
