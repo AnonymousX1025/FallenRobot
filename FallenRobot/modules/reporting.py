a53209a0b9ec6a53720062c70998a25569d07dfa
Anonymous Boy <rahulking25102006@gmail.com>
2022-01-17 11:02:33 +0530

    Update reporting.py

--- FallenRobot/modules/reporting.py
+++ FallenRobot/modules/reporting.py
@@ -285,7 +285,7 @@ dispatcher.add_handler(SETTING_HANDLER)
 dispatcher.add_handler(REPORT_HANDLER, REPORT_GROUP)
 dispatcher.add_handler(ADMIN_REPORT_HANDLER, REPORT_GROUP)
 
-__mod_name__ = "REPORTS"
+__mod_name__ = "ʀᴇᴘᴏʀᴛs​🤨"
 __handlers__ = [
     (REPORT_HANDLER, REPORT_GROUP),
     (ADMIN_REPORT_HANDLER, REPORT_GROUP),
