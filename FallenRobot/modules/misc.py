26ed644061391ae16dcfc188b89bacef4e8bdfec
Anonymous Boy <rahulking25102006@gmail.com>
2022-01-17 11:57:24 +0530

    Update misc.py

--- FallenRobot/modules/misc.py
+++ FallenRobot/modules/misc.py
@@ -111,7 +111,7 @@ MD_HELP_HANDLER = CommandHandler("markdownhelp", markdown_help)
 dispatcher.add_handler(ECHO_HANDLER)
 dispatcher.add_handler(MD_HELP_HANDLER)
 
-__mod_name__ = "EXTRAS"
+__mod_name__ = "ᴇxᴛʀᴀs🤦🏻‍♂️"
 __command_list__ = ["id", "echo"]
 __handlers__ = [
     ECHO_HANDLER,
