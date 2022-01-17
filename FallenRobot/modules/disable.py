437ad10855a238aead8a5b18a7940dcd44e9ad01
Anonymous Boy <rahulking25102006@gmail.com>
2022-01-17 12:02:19 +0530

    Update disable.py

--- FallenRobot/modules/disable.py
+++ FallenRobot/modules/disable.py
@@ -348,7 +348,7 @@ if is_module_loaded(FILENAME):
     ❍ /listcmds*:* list all possible toggleable commands
     """
 
-    __mod_name__ = "DISABLE"
+    __mod_name__ = "ᴅɪsᴀʙʟᴇ🔒"
 
 else:
     DisableAbleCommandHandler = CommandHandler
