03253ed03750f86488f18f40dbf5e6555ef42629
Anonymous Boy <rahulking25102006@gmail.com>
2022-01-17 12:18:19 +0530

    Update chatbot.py

--- FallenRobot/modules/chatbot.py
+++ FallenRobot/modules/chatbot.py
@@ -142,7 +142,7 @@ def list_chatbot_chats(update: Update, context: CallbackContext):
 
 
 __help__ = f"""
-Chatbot utilizes the CoffeeHouse API and allows Saitama to talk and provides a more interactive group chat experience.
+Chatbot utilizes the CoffeeHouse API and allows Fallen to talk and provides a more interactive group chat experience.
 *Commands:* 
 *Admins only:*
  ❍ /addchat*:* Enables Chatbot mode in the chat.
