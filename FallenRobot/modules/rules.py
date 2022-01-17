a92d2e19b184f7a576ce9f5be987a8c64712accd
Anonymous Boy <rahulking25102006@gmail.com>
2022-01-17 09:31:37 +0530

    Update rules.py

--- FallenRobot/modules/rules.py
+++ FallenRobot/modules/rules.py
@@ -1,9 +1,9 @@
 from typing import Optional
 
-import MashaRoBot.modules.sql.rules_sql as sql
-from MashaRoBot import dispatcher
-from MashaRoBot.modules.helper_funcs.chat_status import user_admin
-from MashaRoBot.modules.helper_funcs.string_handling import markdown_parser
+import FallenRobot.modules.sql.rules_sql as sql
+from FallenRobot import dispatcher
+from FallenRobot.modules.helper_funcs.chat_status import user_admin
+from FallenRobot.modules.helper_funcs.string_handling import markdown_parser
 from telegram import (
     InlineKeyboardButton,
     InlineKeyboardMarkup,
