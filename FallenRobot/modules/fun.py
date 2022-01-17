d04db6ef81f6c564ad633137de204babd6e3950d
Anonymous Boy <rahulking25102006@gmail.com>
2022-01-17 11:25:56 +0530

    Update fun.py

--- FallenRobot/modules/fun.py
+++ FallenRobot/modules/fun.py
@@ -371,7 +371,7 @@ dispatcher.add_handler(DECIDE_HANDLER)
 dispatcher.add_handler(EIGHTBALL_HANDLER)
 dispatcher.add_handler(TABLE_HANDLER)
 
-__mod_name__ = "MEMES"
+__mod_name__ = "ᴍᴇᴍᴇs😜"
 __command_list__ = [
     "runs",
     "slap",
