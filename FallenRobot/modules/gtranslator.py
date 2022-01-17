7a2495cf3384d4c86bcf60151c428c7d30f9f563
Anonymous Boy <rahulking25102006@gmail.com>
2022-01-17 11:46:43 +0530

    Update gtranslator.py

--- FallenRobot/modules/gtranslator.py
+++ FallenRobot/modules/gtranslator.py
@@ -109,6 +109,6 @@ TRANSLATE_HANDLER = DisableAbleCommandHandler(["tr", "tl"], totranslate)
 
 dispatcher.add_handler(TRANSLATE_HANDLER)
 
-__mod_name__ = "G-TRANSLATE"
+__mod_name__ = "ɢ-ᴛʀᴀɴs🤔"
 __command_list__ = ["tr", "tl"]
 __handlers__ = [TRANSLATE_HANDLER]
