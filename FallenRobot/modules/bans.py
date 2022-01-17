b544348e03a371d331aceb97efdf74b59f63bdfb
Anonymous Boy <rahulking25102006@gmail.com>
2022-01-17 10:40:01 +0530

    Update bans.py

--- FallenRobot/modules/bans.py
+++ FallenRobot/modules/bans.py
@@ -423,7 +423,7 @@ dispatcher.add_handler(UNBAN_HANDLER)
 dispatcher.add_handler(ROAR_HANDLER)
 dispatcher.add_handler(KICKME_HANDLER)
 
-__mod_name__ = "BANS"
+__mod_name__ = "ʙᴀɴs​🤬"
 __handlers__ = [
     BAN_HANDLER,
     TEMPBAN_HANDLER,
