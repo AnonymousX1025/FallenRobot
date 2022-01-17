32f8634e28c9f762d3266c161590107660bdd8bc
Anonymous Boy <rahulking25102006@gmail.com>
2022-01-17 10:28:18 +0530

    Update blacklist_stickers.py

--- FallenRobot/modules/blacklist_stickers.py
+++ FallenRobot/modules/blacklist_stickers.py
@@ -507,7 +507,7 @@ Note:
  ❍ <sticker link> can be `https://t.me/addstickers/<sticker>` or just `<sticker>` or reply to the sticker message
 """
 
-__mod_name__ = "B STICK"
+__mod_name__ = "ʙʟ-sᴛɪᴄᴋᴇʀ​😑"
 
 BLACKLIST_STICKER_HANDLER = DisableAbleCommandHandler(
     "blsticker", blackliststicker, admin_ok=True
