167a56b855060edeba448ebb9011898f94a35be6
Anonymous Boy <rahulking25102006@gmail.com>
2022-01-18 13:39:00 +0530

    Create __couple.py

---
+++ FallenRobot/modules/__couple.py
@@ -0,0 +1,75 @@
+import random
+from datetime import datetime
+
+from pyrogram import filters
+
+from FallenRobot import pbot
+from FallenRobot.helper_extra.dbfun import get_couple, save_couple
+
+# Date and time
+def dt():
+    now = datetime.now()
+    dt_string = now.strftime("%d/%m/%Y %H:%M")
+    dt_list = dt_string.split(" ")
+    return dt_list
+
+
+def dt_tom():
+    a = (
+        str(int(dt()[0].split("/")[0]) + 1)
+        + "/"
+        + dt()[0].split("/")[1]
+        + "/"
+        + dt()[0].split("/")[2]
+    )
+    return a
+
+
+today = str(dt()[0])
+tomorrow = str(dt_tom())
+
+
+@pbot.on_message(filters.command("couple") & ~filters.edited)
+async def couple(_, message):
+    if message.chat.type == "private":
+        return await message.reply_text("This command only works in groups.")
+    try:
+        chat_id = message.chat.id
+        is_selected = await get_couple(chat_id, today)
+        if not is_selected:
+            list_of_users = []
+            async for i in pbot.iter_chat_members(message.chat.id):
+                if not i.user.is_bot:
+                    list_of_users.append(i.user.id)
+            if len(list_of_users) < 2:
+                return await message.reply_text("Not enough users")
+            c1_id = random.choice(list_of_users)
+            c2_id = random.choice(list_of_users)
+            while c1_id == c2_id:
+                c1_id = random.choice(list_of_users)
+            c1_mention = (await pbot.get_users(c1_id)).mention
+            c2_mention = (await pbot.get_users(c2_id)).mention
+
+            couple_selection_message = f"""**Couple of the day:**
+{c1_mention} + {c2_mention} = 😘
+__New couple of the day may be chosen at 12AM {tomorrow}__"""
+            await pbot.send_message(
+                message.chat.id, text=couple_selection_message
+            )
+            couple = {"c1_id": c1_id, "c2_id": c2_id}
+            await save_couple(chat_id, today, couple)
+
+        elif is_selected:
+            c1_id = int(is_selected["c1_id"])
+            c2_id = int(is_selected["c2_id"])
+            c1_name = (await pbot.get_users(c1_id)).first_name
+            c2_name = (await pbot.get_users(c2_id)).first_name
+            couple_selection_message = f"""Couple of the day:
+[{c1_name}](tg://openmessage?user_id={c1_id}) + [{c2_name}](tg://openmessage?user_id={c2_id}) = 😘
+__New couple of the day may be chosen at 12AM {tomorrow}__"""
+            await pbot.send_message(
+                message.chat.id, text=couple_selection_message
+            )
+    except Exception as e:
+        print(e)
+        await message.reply_text(e)
