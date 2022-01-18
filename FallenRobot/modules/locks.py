274667753bbc91ecbeef749d3e360472fcde0119
Anonymous Boy <rahulking25102006@gmail.com>
2022-01-19 01:08:13 +0530

    Update locks.py

--- FallenRobot/modules/locks.py
+++ FallenRobot/modules/locks.py
@@ -585,7 +585,7 @@ Locking bots will stop non-admins from adding bots to the chat.
  • Unlocking permission *pin* will allow members (non-admins) to pin a message in a group
 """
 
-__mod_name__ = "ʟᴏᴄᴋs🔐"
+__mod_name__ = "Lᴏᴄᴋs🔐"
 
 LOCKTYPES_HANDLER = DisableAbleCommandHandler("locktypes", locktypes)
 LOCK_HANDLER = CommandHandler("lock", lock, pass_args=True)  # , filters=Filters.group)
