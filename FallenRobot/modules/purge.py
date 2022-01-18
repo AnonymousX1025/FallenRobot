cbb3d3cfd86d415bd0cf68535f15faae1117b218
Anonymous Boy <rahulking25102006@gmail.com>
2022-01-19 03:45:11 +0530

    Update purge.py

--- FallenRobot/modules/purge.py
+++ FallenRobot/modules/purge.py
@@ -81,6 +81,6 @@ DEL_HANDLER = delete_messages, events.NewMessage(pattern="^[!/]del$")
 telethn.add_event_handler(*PURGE_HANDLER)
 telethn.add_event_handler(*DEL_HANDLER)
 
-__mod_name__ = "ᴘᴜʀɢᴇ❌"
+__mod_name__ = "Pᴜʀɢᴇ❌"
 __command_list__ = ["del", "purge"]
 __handlers__ = [PURGE_HANDLER, DEL_HANDLER]
