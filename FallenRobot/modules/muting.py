a8cb3314b4d31cb5d2a349bfa58b66d2cef77c0a
Anonymous Boy <rahulking25102006@gmail.com>
2022-01-19 01:12:34 +0530

    Update muting.py

--- FallenRobot/modules/muting.py
+++ FallenRobot/modules/muting.py
@@ -254,5 +254,5 @@ dispatcher.add_handler(MUTE_HANDLER)
 dispatcher.add_handler(UNMUTE_HANDLER)
 dispatcher.add_handler(TEMPMUTE_HANDLER)
 
-__mod_name__ = "ᴍᴜᴛᴇ​🤐"
+__mod_name__ = "Mᴜᴛᴇ​🤐"
 __handlers__ = [MUTE_HANDLER, UNMUTE_HANDLER, TEMPMUTE_HANDLER]
