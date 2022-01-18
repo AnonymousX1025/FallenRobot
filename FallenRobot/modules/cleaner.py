57712891cc80dbf7ea516363faa830017d3d64f0
Anonymous Boy <rahulking25102006@gmail.com>
2022-01-19 01:05:23 +0530

    Update cleaner.py

--- FallenRobot/modules/cleaner.py
+++ FallenRobot/modules/cleaner.py
@@ -250,7 +250,7 @@ dispatcher.add_handler(REMOVE_CLEAN_BLUE_TEXT_GLOBAL_HANDLER)
 dispatcher.add_handler(LIST_CLEAN_BLUE_TEXT_HANDLER)
 dispatcher.add_handler(CLEAN_BLUE_TEXT_HANDLER, BLUE_TEXT_CLEAN_GROUP)
 
-__mod_name__ = "ʙʟᴜᴇᴛᴇxᴛ🌀"
+__mod_name__ = "Bʟᴜᴇᴛᴇxᴛ🌀"
 __handlers__ = [
     SET_CLEAN_BLUE_TEXT_HANDLER,
     ADD_CLEAN_BLUE_TEXT_HANDLER,
