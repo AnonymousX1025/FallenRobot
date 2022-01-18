2bb4252603adc8ad6a77a4acdeb6ab2a05b7d362
Anonymous Boy <rahulking25102006@gmail.com>
2022-01-19 03:20:30 +0530

    Create myinfo.py

---
+++ FallenRobot/modules/myinfo.py
@@ -0,0 +1,82 @@
+from telethon import events, Button, custom, version
+from telethon.tl.types import ChannelParticipantsAdmins
+import asyncio
+import os,re
+import requests
+import datetime
+import time
+from datetime import datetime
+import random
+from PIL import Image
+from io import BytesIO
+from FallenRobot import telethn as bot
+from FallenRobot import telethn as tgbot
+from FallenRobot.events import register
+from FallenRobot import dispatcher
+
+
+edit_time = 5
+""" =======================FALLEN ROBOT====================== """
+file1 = "https://telegra.ph/file/72e135da30a1a349f2844.jpg"
+file2 = "https://telegra.ph/file/722063a466b3f4f03942c.jpg"
+file3 = "https://telegra.ph/file/862bf2d97058a6017215b.jpg"
+file4 = "https://telegra.ph/file/93b08120e36d096cd21ca.jpg"
+file5 = "https://telegra.ph/file/f6f020ef5aeeb079b15b9.jpg"
+""" =======================FALLEN ROBOT====================== """
+
+
+@register(pattern="/myinfo")
+async def proboyx(event):
+    chat = await event.get_chat()
+    current_time = datetime.utcnow()
+    firstname = event.sender.first_name
+    button = [[custom.Button.inline("information",data="informations")]]
+    on = await bot.send_file(event.chat_id, file=file2,caption= f"hello {firstname}, \n Click The Below Button \n To Get Your Info", buttons=button)
+
+    await asyncio.sleep(edit_time)
+    ok = await bot.edit_message(event.chat_id, on, file=file3, buttons=button) 
+
+    await asyncio.sleep(edit_time)
+    ok2 = await bot.edit_message(event.chat_id, ok, file=file5, buttons=button)
+
+    await asyncio.sleep(edit_time)
+    ok3 = await bot.edit_message(event.chat_id, ok2, file=file1, buttons=button)
+
+    await asyncio.sleep(edit_time)
+    ok7 = await bot.edit_message(event.chat_id, ok6, file=file4, buttons=button)
+    
+    await asyncio.sleep(edit_time)
+    ok4 = await bot.edit_message(event.chat_id, ok3, file=file2, buttons=button)
+    
+    await asyncio.sleep(edit_time)
+    ok5 = await bot.edit_message(event.chat_id, ok4, file=file1, buttons=button)
+    
+    await asyncio.sleep(edit_time)
+    ok6 = await bot.edit_message(event.chat_id, ok5, file=file3, buttons=button)
+    
+    await asyncio.sleep(edit_time)
+    ok7 = await bot.edit_message(event.chat_id, ok6, file=file5, buttons=button)
+
+    await asyncio.sleep(edit_time)
+    ok7 = await bot.edit_message(event.chat_id, ok6, file=file4, buttons=button)
+
+@tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"information")))
+async def callback_query_handler(event):
+  try:
+    boy = event.sender_id
+    PRO = await bot.get_entity(boy)
+    LILIE = "POWERED BY 𝝙𝗡𝗢𝗡𝗬𝗠𝗢𝗨𝗦 \n\n"
+    LILIE += f"FIRST NAME : {PRO.first_name} \n"
+    LILIE += f"LAST NAME : {PRO.last_name}\n"
+    LILIE += f"YOU BOT : {PRO.bot} \n"
+    LILIE += f"RESTRICTED : {PRO.restricted} \n"
+    LILIE += f"USER ID : {boy}\n"
+    LILIE += f"USERNAME : {PRO.username}\n"
+    await event.answer(LILIE, alert=True)
+  except Exception as e:
+    await event.reply(f"{e}")
+
+
+__command_list__ = [
+    "myinfo"
+]
