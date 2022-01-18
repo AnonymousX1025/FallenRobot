2a36425247fb4d17b5828e69f31bd3834ddae412
Anonymous Boy <rahulking25102006@gmail.com>
2022-01-19 01:33:30 +0530

    Update _pyrogithub.py

--- FallenRobot/modules/_pyrogithub.py
+++ FallenRobot/modules/_pyrogithub.py
@@ -5,7 +5,7 @@ from FallenRobot import pbot
 from FallenRobot.pyrogramee.errors import capture_err
 
 
-__mod_name__ = "ɢɪᴛʜᴜʙ🧧"
+__mod_name__ = "Gɪᴛʜᴜʙ🧧"
 
 __help__ = """
 I will give information about github profile 
