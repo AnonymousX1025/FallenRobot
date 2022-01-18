0b426f16de1cdb877b833bc1fad1518c4288cd1e
Anonymous Boy <rahulking25102006@gmail.com>
2022-01-18 15:48:46 +0530

    Update userinfo.py

--- FallenRobot/modules/userinfo.py
+++ FallenRobot/modules/userinfo.py
@@ -517,11 +517,6 @@ def __user_info__(user_id):
 
 
 __help__ = """
-*Away from group*
- ❍ /afk <reason>*:* mark yourself as AFK(away from keyboard).
- ❍ brb <reason>*:* same as the afk command - but not a command.
-When marked as AFK, any mentions will be replied to with a message to say you're not available!
-
 *ID:*
  ❍ /id*:* get the current group id. If used by replying to a message, gets that user's id.
  ❍ /gifid*:* reply to a gif to me to tell you its file ID.
