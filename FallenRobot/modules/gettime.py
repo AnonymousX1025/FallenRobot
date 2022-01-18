e89b13f528d075d87c2d436ab67f1031de17e316
Anonymous Boy <rahulking25102006@gmail.com>
2022-01-19 03:44:11 +0530

    Update gettime.py

--- FallenRobot/modules/gettime.py
+++ FallenRobot/modules/gettime.py
@@ -103,6 +103,6 @@ TIME_HANDLER = DisableAbleCommandHandler("time", gettime)
 
 dispatcher.add_handler(TIME_HANDLER)
 
-__mod_name__ = "ᴛɪᴍᴇ🕕"
+__mod_name__ = "Tɪᴍᴇ🕕"
 __command_list__ = ["time"]
 __handlers__ = [TIME_HANDLER]
