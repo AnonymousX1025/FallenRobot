d46c976d0c44c18881a35c5589ace41e4cf98f06
Anonymous Boy <rahulking25102006@gmail.com>
2022-01-23 14:44:31 +0530

    Update __main__.py

--- FallenRobot/__main__.py
+++ FallenRobot/__main__.py
@@ -670,7 +670,7 @@ def main():
 
     if SUPPORT_CHAT is not None and isinstance(SUPPORT_CHAT, str):
         try:
-            dispatcher.bot.sendMessage(f"@{SUPPORT_CHAT}", "🖤 ɪ ᴀᴍ ᴀʟɪᴠᴇ ʙᴀʙʏ ! 🖤")
+            dispatcher.bot.sendMessage(f"@{SUPPORT_CHAT}", "🖤 ʟᴇᴛ's ꜰᴜ*ᴋ ʙᴀʙʏ​ ! 🤤")
         except Unauthorized:
             LOGGER.warning(
                 "Bot isnt able to send message to support_chat, go and check!"
