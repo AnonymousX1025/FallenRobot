f706d8a692a09252bf90ccbc11cf0e90bb8a76f3
Anonymous Boy <rahulking25102006@gmail.com>
2022-01-24 22:36:37 +0530

    Update __google.py

--- FallenRobot/modules/__google.py
+++ FallenRobot/modules/__google.py
@@ -275,13 +275,13 @@ async def apk(e):
         await e.reply("Exception Occured:- " + str(err))
 
 
-__mod_name__ = "Gᴏᴏɢʟᴇ🌐"
+__mod_name__ = "Gᴏᴏɢʟᴇ"
 
 __help__ = """
  ❍ /google <text>*:* Perform a google search
  ❍ /img <text>*:* Search Google for images and returns them\nFor greater no. of results specify lim, For eg: `/img hello lim=10`
  ❍ /app <appname>*:* Searches for an app in Play Store and returns its details.
  ❍ /reverse: Does a reverse image search of the media which it was replied to.
- ❍ Fallen <query>*:* Fallen answers the query
-  💡Ex: `Fallen where is India?`
+ ❍ Anonymous <query>*:* Fallen answers the query
+  💡Ex: `Anonymous where is India?`
 """
