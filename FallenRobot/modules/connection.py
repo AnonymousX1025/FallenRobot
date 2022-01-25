4d1a397e82855bde2bc58fc26a645376a7c0c4e4
Anonymous Boy <rahulking25102006@gmail.com>
2022-01-25 10:17:06 +0530

    Update connection.py

--- FallenRobot/modules/connection.py
+++ FallenRobot/modules/connection.py
@@ -386,7 +386,7 @@ def connect_button(update, context):
         connect_chat(update, context)
 
 
-__mod_name__ = "Cᴏɴɴᴇᴄᴛ🔗"
+__mod_name__ = "Cᴏɴɴᴇᴄᴛ"
 
 __help__ = """
 Sometimes, you just want to add some notes and filters to a group chat, but you don't want everyone to see; This is where connections come in...
