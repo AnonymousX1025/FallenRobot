00ea87be2cce1b0b7886268e85f3c0f4595000bf
Anonymous Boy <rahulking25102006@gmail.com>
2022-01-25 12:49:56 +0530

    Update stickers.py

--- FallenRobot/modules/stickers.py
+++ FallenRobot/modules/stickers.py
@@ -456,7 +456,7 @@ __help__ = """
  ❍ /stickers*:* Find stickers for given term on combot sticker catalogue
 """
 
-__mod_name__ = "Sᴛɪᴄᴋᴇʀ🖼"
+__mod_name__ = "Sᴛɪᴄᴋᴇʀ"
 STICKERID_HANDLER = DisableAbleCommandHandler("stickerid", stickerid)
 GETSTICKER_HANDLER = DisableAbleCommandHandler("getsticker", getsticker)
 KANG_HANDLER = DisableAbleCommandHandler("kang", kang, admin_ok=True)
