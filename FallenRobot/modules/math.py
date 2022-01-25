415f62b21e385498d0b41d0b5bf813afeaf4e917
Anonymous Boy <rahulking25102006@gmail.com>
2022-01-25 12:30:20 +0530

    Update math.py

--- FallenRobot/modules/math.py
+++ FallenRobot/modules/math.py
@@ -136,7 +136,7 @@ To find the area under a function, send the request as c:d|f(x) where c is the s
 To compute fractions, enter expressions as numerator(over)denominator. For example, to process 2/4 you must send in your expression as 2(over)4. The result expression will be in standard math notation (1/2, 3/4).
 """
 
-__mod_name__ = "Mᴀᴛʜs🤓"
+__mod_name__ = "Mᴀᴛʜs"
 
 SIMPLIFY_HANDLER = DisableAbleCommandHandler("math", simplify)
 FACTOR_HANDLER = DisableAbleCommandHandler("factor", factor)
