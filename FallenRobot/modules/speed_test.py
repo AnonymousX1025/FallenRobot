ccb6d55b1fd579ba75ef00fb5f2e07b201516d4c
Anonymous Boy <rahulking25102006@gmail.com>
2022-01-25 12:49:04 +0530

    Update speed_test.py

--- FallenRobot/modules/speed_test.py
+++ FallenRobot/modules/speed_test.py
@@ -59,6 +59,10 @@ SPEED_TEST_CALLBACKHANDLER = CallbackQueryHandler(
 dispatcher.add_handler(SPEED_TEST_HANDLER)
 dispatcher.add_handler(SPEED_TEST_CALLBACKHANDLER)
 
-__mod_name__ = "SpeedTest"
+__help__ = """
+» /speedtest *:* Runs a speedtest and check the server speed.
+"""
+
+__mod_name__ = "SᴘᴇᴇᴅTᴇsᴛ​"
 __command_list__ = ["speedtest"]
 __handlers__ = [SPEED_TEST_HANDLER, SPEED_TEST_CALLBACKHANDLER]
