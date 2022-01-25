c8aebbb49aa0114e460055a6ae16c2dcdf058769
Anonymous Boy <rahulking25102006@gmail.com>
2022-01-25 12:41:25 +0530

    Update rules.py

--- FallenRobot/modules/rules.py
+++ FallenRobot/modules/rules.py
@@ -125,7 +125,7 @@ __help__ = """
  ❍ /clearrules*:* clear the rules for this chat.
 """
 
-__mod_name__ = "Rᴜʟᴇs😏"
+__mod_name__ = "Rᴜʟᴇs"
 
 GET_RULES_HANDLER = CommandHandler("rules", get_rules, filters=Filters.group)
 SET_RULES_HANDLER = CommandHandler("setrules", set_rules, filters=Filters.group)
