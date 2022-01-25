fbd7aad78b7a40ef758efe747b1902de68eafc0a
Anonymous Boy <rahulking25102006@gmail.com>
2022-01-25 12:55:16 +0530

    Update ud.py

--- FallenRobot/modules/ud.py
+++ FallenRobot/modules/ud.py
@@ -23,5 +23,9 @@ UD_HANDLER = DisableAbleCommandHandler(["ud"], ud)
 
 dispatcher.add_handler(UD_HANDLER)
 
+__help__ = """
+» /ud (text) *:* Searchs the given text on Urban Dictionary and sends you the information.
+"""
+__mod_name__ = "Uʀʙᴀɴ D"
 __command_list__ = ["ud"]
 __handlers__ = [UD_HANDLER]
