6e85b44fa02515f7bbe275afe5393a06c87e8fba
Anonymous Boy <rahulking25102006@gmail.com>
2022-01-25 10:03:29 +0530

    Update afk.py

--- FallenRobot/modules/afk.py
+++ FallenRobot/modules/afk.py
@@ -157,7 +157,7 @@ dispatcher.add_handler(AFK_REGEX_HANDLER, AFK_GROUP)
 dispatcher.add_handler(NO_AFK_HANDLER, AFK_GROUP)
 dispatcher.add_handler(AFK_REPLY_HANDLER, AFK_REPLY_GROUP)
 
-__mod_name__ = "Aꜰᴋ​💤"
+__mod_name__ = "Aꜰᴋ​"
 __command_list__ = ["afk"]
 __handlers__ = [
     (AFK_HANDLER, AFK_GROUP),
