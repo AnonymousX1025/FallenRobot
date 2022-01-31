7e69e8767f0e66ad6893cec496d9bbbca403d5e2
Anonymous Boy <rahulking25102006@gmail.com>
2022-01-31 23:31:30 +0530

    Update logo.py

--- FallenRobot/modules/logo.py
+++ FallenRobot/modules/logo.py
@@ -9,7 +9,7 @@ API_ID = int(os.environ.get("API_ID"))
 API_HASH = os.environ.get("API_HASH")
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
 
-app = Client("logo Bot", bot_token = BOT_TOKEN, api_id = API_ID, api_hash = API_HASH)
+app = Client("Fallen Robot", bot_token = BOT_TOKEN, api_id = API_ID, api_hash = API_HASH)
 
 app.run()
 
