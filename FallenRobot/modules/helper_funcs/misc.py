1bf59bcdc420f14629ceb9aed04d38406d23f5b1
Snehashish06 <95462709+Snehashish06@users.noreply.github.com>
2022-02-01 00:56:22 +0530

    Update misc.py

--- FallenRobot/modules/helper_funcs/misc.py
+++ FallenRobot/modules/helper_funcs/misc.py
@@ -1,8 +1,19 @@
+from math import ceil
 from typing import Dict, List
+from uuid import uuid4
+
+from telegram import (
+    MAX_MESSAGE_LENGTH,
+    Bot,
+    InlineKeyboardButton,
+    InlineKeyboardMarkup,
+    InlineQueryResultArticle,
+    InputTextMessageContent,
+    ParseMode,
+)
+from telegram.error import TelegramError
 
 from FallenRobot import NO_LOAD
-from telegram import MAX_MESSAGE_LENGTH, Bot, InlineKeyboardButton, ParseMode
-from telegram.error import TelegramError
 
 
 class EqInlineKeyboardButton(InlineKeyboardButton):
@@ -62,19 +73,56 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
             ]
         )
 
-    pairs = [modules[i * 4 : (i + 1) * 4] for i in range((len(modules) + 4 - 1) // 4)]
+    pairs = [modules[i * 3 : (i + 1) * 3] for i in range((len(modules) + 5 - 1) // 3)]
 
-    round_num = len(modules) / 4
+    round_num = len(modules) / 3
     calc = len(modules) - round(round_num)
     if calc in [1, 2]:
         pairs.append((modules[-1],))
-    elif calc == 2:
-        pairs.append((modules[-1],))
+
+    max_num_pages = ceil(len(pairs) / 3)
+    modulo_page = page_n % max_num_pages
+
+    # can only have a certain amount of buttons side by side
+    if len(pairs) > 3:
+        pairs = pairs[modulo_page * 3 : 5 * (modulo_page + 1)] + [
+            (
+                EqInlineKeyboardButton(
+                    "⏎", callback_data="{}_prev({})".format(prefix, modulo_page)
+                ),
+                EqInlineKeyboardButton("⨭", callback_data="masha_back"),
+                EqInlineKeyboardButton(
+                    "➽", callback_data="{}_next({})".format(prefix, modulo_page)
+                ),
+            )
+        ]
 
     else:
-        pairs += [[EqInlineKeyboardButton("「 GO BACK TO MAIN MENU 」", callback_data="fallen_back")]]
+        pairs += [[EqInlineKeyboardButton("⨭", callback_data="masha_back")]]
 
     return pairs
+
+
+def article(
+    title: str = "",
+    description: str = "",
+    message_text: str = "",
+    thumb_url: str = None,
+    reply_markup: InlineKeyboardMarkup = None,
+    disable_web_page_preview: bool = False,
+) -> InlineQueryResultArticle:
+
+    return InlineQueryResultArticle(
+        id=uuid4(),
+        title=title,
+        description=description,
+        thumb_url=thumb_url,
+        input_message_content=InputTextMessageContent(
+            message_text=message_text,
+            disable_web_page_preview=disable_web_page_preview,
+        ),
+        reply_markup=reply_markup,
+    )
 
 
 def send_to_list(
@@ -125,6 +173,29 @@ def build_keyboard_parser(bot, chat_id, buttons):
             keyb[-1].append(InlineKeyboardButton(btn.name, url=btn.url))
         else:
             keyb.append([InlineKeyboardButton(btn.name, url=btn.url)])
+
+    return keyb
+
+
+def user_bot_owner(func):
+    @wraps(func)
+    def is_user_bot_owner(bot: Bot, update: Update, *args, **kwargs):
+        user = update.effective_user
+        if user and user.id == OWNER_ID:
+            return func(bot, update, *args, **kwargs)
+        else:
+            pass
+
+    return is_user_bot_owner
+
+
+def build_keyboard_alternate(buttons):
+    keyb = []
+    for btn in buttons:
+        if btn[2] and keyb:
+            keyb[-1].append(InlineKeyboardButton(btn[0], url=btn[1]))
+        else:
+            keyb.append([InlineKeyboardButton(btn[0], url=btn[1])])
 
     return keyb
 
