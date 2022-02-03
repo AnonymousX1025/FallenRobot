36b5eb6b13c7d2bb8072b80652a51e60efa0f4d2
Anonymous <rahulking25102006@gmail.com>
2022-02-04 02:27:22 +0530

    Update misc.py

--- FallenRobot/modules/helper_funcs/misc.py
+++ FallenRobot/modules/helper_funcs/misc.py
@@ -88,17 +88,17 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
         pairs = pairs[modulo_page * 3 : 3 * (modulo_page + 1)] + [
             (
                 EqInlineKeyboardButton(
-                    "⏎", callback_data="{}_prev({})".format(prefix, modulo_page)
+                    "◁", callback_data="{}_prev({})".format(prefix, modulo_page)
                 ),
-                EqInlineKeyboardButton("⨭", callback_data="fallen_back"),
+                EqInlineKeyboardButton("• ʙᴀᴄᴋ •", callback_data="fallen_back"),
                 EqInlineKeyboardButton(
-                    "➽", callback_data="{}_next({})".format(prefix, modulo_page)
+                    "▷", callback_data="{}_next({})".format(prefix, modulo_page)
                 ),
             )
         ]
 
     else:
-        pairs += [[EqInlineKeyboardButton("⨭", callback_data="fallen_back")]]
+        pairs += [[EqInlineKeyboardButton("• ʙᴀᴄᴋ •", callback_data="fallen_back")]]
 
     return pairs
 
