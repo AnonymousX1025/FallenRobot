c5e610ed4719c6c1565d13376c34e08f9e0c5cac
Anonymous <rahulking25102006@gmail.com>
2022-02-16 17:35:56 +0530

    Update paste.py

--- FallenRobot/modules/paste.py
+++ FallenRobot/modules/paste.py
@@ -1,43 +1,72 @@
-import requests
-from FallenRobot import dispatcher
-from FallenRobot.modules.disable import DisableAbleCommandHandler
-from telegram import ParseMode, Update
-from telegram.ext import CallbackContext, run_async
+import asyncio
+import os
+import re
+
+import aiofiles
+from pykeyboard import InlineKeyboard
+from pyrogram import filters
+from pyrogram.types import InlineKeyboardButton
+
+from FallenRobot import ubot2, aiohttpsession
+from FallenRobot.utils.errors import capture_err
+from FallenRobot.utils.pastebin import paste
+
+pattern = re.compile(
+    r"^text/|json$|yaml$|xml$|toml$|x-sh$|x-shellscript$"
+)
 
 
-@run_async
-def paste(update: Update, context: CallbackContext):
-    args = context.args
-    message = update.effective_message
-
-    if message.reply_to_message:
-        data = message.reply_to_message.text
-
-    elif len(args) >= 1:
-        data = message.text.split(None, 1)[1]
-
-    else:
-        message.reply_text("What am I supposed to do with this?")
-        return
-
-    key = (
-        requests.post("https://nekobin.com/api/documents", json={"content": data})
-        .json()
-        .get("result")
-        .get("key")
-    )
-
-    url = f"https://nekobin.com/{key}"
-
-    reply_text = f"Nekofied to *Nekobin* : {url}"
-
-    message.reply_text(
-        reply_text, parse_mode=ParseMode.MARKDOWN, disable_web_page_preview=True
-    )
+async def isPreviewUp(preview: str) -> bool:
+    for _ in range(7):
+        try:
+            async with aiohttpsession.head(preview, timeout=2) as resp:
+                status = resp.status
+                size = resp.content_length
+        except asyncio.exceptions.TimeoutError:
+            return False
+        if status == 404 or (status == 200 and size == 0):
+            await asyncio.sleep(0.4)
+        else:
+            return status == 200
+    return False
 
 
-PASTE_HANDLER = DisableAbleCommandHandler("paste", paste)
-dispatcher.add_handler(PASTE_HANDLER)
+@pgram.on_message(filters.command("paste") & ~filters.edited)
+@capture_err
+async def paste_func(_, message):
+    if not message.reply_to_message:
+        return await message.reply_text(
+            "Reply To A Message With /paste"
+        )
+    m = await message.reply_text("Pasting...")
+    if message.reply_to_message.text:
+        content = str(message.reply_to_message.text)
+    elif message.reply_to_message.document:
+        document = message.reply_to_message.document
+        if document.file_size > 1048576:
+            return await m.edit(
+                "You can only paste files smaller than 1MB."
+            )
+        if not pattern.search(document.mime_type):
+            return await m.edit("Only text files can be pasted.")
+        doc = await message.reply_to_message.download()
+        async with aiofiles.open(doc, mode="r") as f:
+            content = await f.read()
+        os.remove(doc)
+    link = await paste(content)
+    preview = link + "/preview.png"
+    button = InlineKeyboard(row_width=1)
+    button.add(InlineKeyboardButton(text="ᴘᴀsᴛᴇ ʟɪɴᴋ​", url=link))
+
+    if await isPreviewUp(preview):
+        try:
+            await message.reply_photo(
+                photo=preview, quote=False, reply_markup=button
+            )
+            return await m.delete()
+        except Exception:
+            pass
+    return await m.edit(link)
 
 
 __help__ = """
@@ -46,4 +75,3 @@ __help__ = """
 """
 __mod_name__ = "Pᴀsᴛᴇ​"
 __command_list__ = ["paste"]
-__handlers__ = [PASTE_HANDLER]
