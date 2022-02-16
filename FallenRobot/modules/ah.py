250e87dd13830bdb5f0bc4b8b54cd3f5b1276b34
Anonymous <rahulking25102006@gmail.com>
2022-02-16 21:33:07 +0530

    Update ah.py

--- FallenRobot/modules/ah.py
+++ FallenRobot/modules/ah.py
@@ -3,14 +3,15 @@ from pyrogram import filters
 from FallenRobot import pbot as client
 
 
-@client.on_message(filters.command("owner"))
+@client.on_message(filters.command("dev"))
 async def start(client, message):
     await message.reply(
-            "Here is your gf \nBsdk Girlfriend nahi \nGodFather 😂",
+            "ʜᴇʏ\n  ɪ ᴀᴍ 𝗙𝝙𝗟𝗟𝝣𝗡 ✘ 𝗥𝗢𝗕𝗢𝗧\n    ᴍʏ ᴅᴇᴠᴇʟᴏᴘᴇʀ​ ɪs​‌ [𝝙𝗡𝗢𝗡𝗬𝗠𝗢𝗨𝗦](tg://user?id=1356469075)",
             reply_markup=InlineKeyboardMarkup([
-                [InlineKeyboardButton(" Anonymous", url="https://t.me/anonymous_was_bot")]
+                [InlineKeyboardButton("✗ 𝝙𝗡𝗢𝗡𝗬𝗠𝗢𝗨𝗦 ✗", url="https://t.me/anonymous_was_bot")]
                 ]
             )
         )
 
-## invisible Module 😂
+## Ramdi Module  😂
+__mod_name__ = "Ramdi"
