a3dd28590dfc93eb32045c4f120288f6e9dda3cc
Affectionate <95462709+Snehashish06@users.noreply.github.com>
2022-02-16 22:41:11 +0800

    Update ramdi.py

--- FallenRobot/modules/ramdi.py
+++ FallenRobot/modules/ramdi.py
@@ -1,4 +1,4 @@
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton,
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from FallenRobot import pbot as fallen
 
 
