f981528c3898acc8f6b7b139a4420c1fc45d93e1
Affectionate <95462709+Snehashish06@users.noreply.github.com>
2022-02-18 21:06:12 +0530

    Update logo.py

--- FallenRobot/modules/logo.py
+++ FallenRobot/modules/logo.py
@@ -7,10 +7,8 @@ import requests
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from FallenRobot import pbot as app
 from FallenRobot.sample_config import (
-                                        API_ID, 
-                                        API_HASH, 
-                                        BOT_TOKEN,
-                                        )
+                          API_HASH, 
+                          BOT_TOKEN,                          )
 
 API_ID = int(os.environ.get("API_ID"))
 API_HASH = os.environ.get("API_HASH")
