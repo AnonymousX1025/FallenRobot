096f4e3cf9e66f1d76f64ce78f3a1bed16ee6d9f
Affectionate <95462709+Snehashish06@users.noreply.github.com>
2022-03-05 01:09:38 +0530

    Update admin.py

--- FallenRobot/modules/admin.py
+++ FallenRobot/modules/admin.py
@@ -2,7 +2,7 @@ import html
 
 from telegram import ParseMode, Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.error import BadRequest
-from telegram.ext import CallbackContext, CommandHandler, Filters, run_async
+from telegram.ext import CallbackContext, CommandHandler, Filters, chat_type, run_async
 from telegram.utils.helpers import mention_html
 
 from FallenRobot import DRAGONS, dispatcher
