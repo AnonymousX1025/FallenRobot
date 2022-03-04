879f70beec9cdcb2f44b8ea2b38e4856c7fcb343
Anonymous <rahulking25102006@gmail.com>
2022-03-05 03:39:38 +0530

    Update handlers.py

--- FallenRobot/modules/helper_funcs/handlers.py
+++ FallenRobot/modules/helper_funcs/handlers.py
@@ -13,7 +13,7 @@ from pyrate_limiter import (
 )
 
 if ALLOW_EXCL:
-    CMD_STARTERS = ("/", "!", ".", "•", "♡", "^")
+    CMD_STARTERS = ("/", "!", ".", "•", "♡", "~")
 else:
     CMD_STARTERS = ("/",)
 
