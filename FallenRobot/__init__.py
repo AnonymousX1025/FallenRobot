179ceddc2233965a07afd350393b241d619e3f91
Affectionate <95462709+Snehashish06@users.noreply.github.com>
2022-03-05 01:39:09 +0530

    Update __init__.py

--- FallenRobot/__init__.py
+++ FallenRobot/__init__.py
@@ -19,6 +19,8 @@ logging.basicConfig(
 
 LOGGER = logging.getLogger(__name__)
 
+x = pbot.get_me()
+
 # if version < 3.6, stop bot.
 if sys.version_info[0] < 3 or sys.version_info[1] < 6:
     LOGGER.error(
