81b6b179bdf9d38fdf145e8b8eb759395da5d63d
Affectionate <95462709+Snehashish06@users.noreply.github.com>
2022-03-05 02:28:43 +0530

    Update ffilter.py

--- FallenRobot/modules/ffilter.py
+++ FallenRobot/modules/ffilter.py
@@ -25,8 +25,6 @@ SFW_STRINGS = (
 
 @run_async
 def dark(bot: Bot, update: Update):
-    bot.sendChatAction(update.effective_chat.id, "typing") # Bot typing before send messages
-    message = update.effective_message
     if message.reply_to_message:
       message.reply_to_message.reply_text(random.choice(SFW_STRINGS))
     else:
