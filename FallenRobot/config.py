1dd459e72ff6bd82142fb59e2975c841e8dd80af
Affectionate <95462709+Snehashish06@users.noreply.github.com>
2022-03-07 21:56:30 +0530

    Update config.py

--- FallenRobot/config.py
+++ FallenRobot/config.py
@@ -5,4 +5,4 @@ class Config(object):
     API_ID = int(os.environ.get("API_ID", "1234"))
     API_HASH = os.environ.get("API_HASH")
     BOT_TOKEN = os.environ.get("BOT_TOKEN")
-    DATABASE_URL = os.environ.get("DATABASE_URL")
+    DATABASE_URL = os.environ.get("DATABASE_URLL")
