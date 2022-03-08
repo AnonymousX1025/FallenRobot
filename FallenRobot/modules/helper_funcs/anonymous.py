53984772c30f4c2660174a88e50454ae4f1b6fc8
Affectionate <95462709+Snehashish06@users.noreply.github.com>
2022-03-08 22:03:31 +0530

    Update anonymous.py

--- FallenRobot/modules/helper_funcs/anonymous.py
+++ FallenRobot/modules/helper_funcs/anonymous.py
@@ -61,7 +61,7 @@ def user_admin(permission: AdminPerms):
     return wrapper
 
 
-@kigcallback(pattern="anoncb")
+@fallencallback(pattern="anoncb")
 def anon_callback_handler1(upd: Update, _: CallbackContext):
     callback = upd.callback_query
     perm = callback.data.split('/')[3]
