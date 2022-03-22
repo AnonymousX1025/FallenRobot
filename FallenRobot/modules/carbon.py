adbddc323d282e9663f768fb37a3dd900f936635
Anonymous <rahulking25102006@gmail.com>
2022-03-22 12:25:01 +0530

    Create carbon.py

---
+++ FallenRobot/modules/carbon.py
@@ -0,0 +1,53 @@
+from platform import python_version as y
+from telegram import __version__ as o
+from pyrogram import __version__ as z
+from telethon import __version__ as s
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram import filters
+from FallenRobot import pbot
+from FallenRobot.utils.errors import capture_err
+from FallenRobot.utils.functions import make_carbon
+
+
+@pbot.on_message(filters.command("carbon"))
+@capture_err
+async def carbon_func(_, message):
+    if not message.reply_to_message:
+        return await message.reply_text("`ʀᴇᴩʟʏ ᴛᴏ ᴀ ᴛᴇxᴛ ᴛᴏ ɢᴇɴᴇʀᴀᴛᴇ ᴄᴀʀʙᴏɴ.`")
+    if not message.reply_to_message.text:
+        return await message.reply_text("`ʀᴇᴩʟʏ ᴛᴏ ᴀ ᴛᴇxᴛ ᴛᴏ ɢᴇɴᴇʀᴀᴛᴇ ᴄᴀʀʙᴏɴ.`")
+    m = await message.reply_text("😴`ɢᴇɴᴇʀᴀᴛɪɴɢ ᴄᴀʀʙᴏɴ...`")
+    carbon = await make_carbon(message.reply_to_message.text)
+    await m.edit("`ᴜᴩʟᴏᴀᴅɪɴɢ ɢᴇɴᴇʀᴀᴛᴇᴅ ᴄᴀʀʙᴏɴ...`")
+    await pbot.send_photo(message.chat.id, carbon)
+    await m.delete()
+    carbon.close()
+
+
+ANON = "https://telegra.ph/file/ceb116e569f2255fedb6d.jpg"
+
+@pbot.on_message(filters.command(["repo", "source"]))
+async def repo(_, message):
+    await message.reply_photo(
+        photo=ANON,
+        caption=f"""**ʜᴇʏ​ [{event.sender.first_name}](tg://user?id={event.sender.id}),\n\nɪ ᴀᴍ ꜰᴀʟʟᴇɴ ✘ ʀᴏʙᴏᴛ-🇮🇩.**
+
+**» ᴍʏ ᴅᴇᴠᴇʟᴏᴘᴇʀ​ :** [𝝙𝗡𝗢𝗡𝗬𝗠𝗢𝗨𝗦](tg://user?id=1356469075)
+**» ᴩʏᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `{y()}`
+**» ʟɪʙʀᴀʀʏ ᴠᴇʀsɪᴏɴ :** `{o}` 
+**» ᴛᴇʟᴇᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `{s}` 
+**» ᴘʏʀᴏɢʀᴀᴍ ᴠᴇʀsɪᴏɴ :** `{z}`
+
+**ꜰᴀʟʟᴇɴ ✘ ʀᴏʙᴏᴛ sᴏᴜʀᴄᴇ ɪs ɴᴏᴡ ᴩᴜʙʟɪᴄ ᴀɴᴅ ɴᴏᴡ ʏᴏᴜ ᴄᴀɴ ᴍᴀᴋᴇ ʏᴏᴜʀ ᴏᴡɴ ʙᴏᴛ.**
+""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "• ᴏᴡɴᴇʀ •", url="tg://user?id=1356469075"), 
+                    InlineKeyboardButton(
+                        "• sᴏᴜʀᴄᴇ •", url="https://github.com/AnonymousBoy1025/FallenRobot")
+                ]
+            ]
+        )
+    )
