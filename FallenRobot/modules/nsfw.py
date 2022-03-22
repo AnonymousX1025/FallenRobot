31325fa7fd3255b06fed0ffd73b21c313b737a10
Anonymous <rahulking25102006@gmail.com>
2022-03-22 13:25:56 +0530

    Update nsfw.py

--- FallenRobot/modules/nsfw.py
+++ FallenRobot/modules/nsfw.py
@@ -813,5 +813,62 @@ __handlers__ = [
 ]
 
 
+__help__ = """
+*NSFW:*
+/addnsfw : Enable NSFW mode
+/rmnsfw : Disable NSFW mode
+ 
+*Available commands:*  
+ - /neko: Sends Random SFW Neko source Images.
+ - /feet: Sends Random Anime Feet Images.
+ - /yuri: Sends Random Yuri source Images.
+ - /trap: Sends Random Trap source Images.
+ - /futanari: Sends Random Futanari source Images.
+ - /hololewd: Sends Random Holo Lewds.
+ - /lewdkemo: Sends Random Kemo Lewds.
+ - /sologif: Sends Random Solo GIFs.
+ - /cumgif: Sends Random Cum GIFs.
+ - /erokemo: Sends Random Ero-Kemo Images.
+ - /lesbian: Sends Random Les Source Images.
+ - /lewdk: Sends Random Kitsune Lewds.
+ - /ngif: Sends Random Neko GIFs.
+ - /tickle: Sends Random Tickle GIFs.
+ - /lewd: Sends Random Lewds.
+ - /feed: Sends Random Feeding GIFs.
+ - /eroyuri: Sends Random Ero-Yuri source Images.
+ - /eron: Sends Random Ero-Neko source Images.
+ - /cum: Sends Random Cum Images.
+ - /bjgif: Sends Random Blow Job GIFs.
+ - /bj: Sends Random Blow Job source Images.
+ - /nekonsfw: Sends Random NSFW Neko source Images.
+ - /solo: Sends Random NSFW Neko GIFs.
+ - /kemonomimi: Sends Random KemonoMimi source Images.
+ - /avatarlewd: Sends Random Avater Lewd Stickers.
+ - /gasm: Sends Random Orgasm Stickers.
+ - /poke: Sends Random Poke GIFs.
+ - /anal: Sends Random Anal GIFs.
+ - /hentai: Sends Random Hentai source Images.
+ - /avatar: Sends Random Avatar Stickers.
+ - /erofeet: Sends Random Ero-Feet source Images.
+ - /holo: Sends Random Holo source Images.
+ - /tits: Sends Random Tits source Images.
+ - /pussygif: Sends Random Pussy GIFs.
+ - /holoero: Sends Random Ero-Holo source Images.
+ - /pussy: Sends Random Pussy source Images.
+ - /hentaigif: Sends Random Hentai GIFs.
+ - /classic: Sends Random Classic Hentai GIFs.
+ - /kuni: Sends Random Pussy Lick GIFs.
+ - /waifu: Sends Random Waifu Stickers.
+ - /kiss: Sends Random Kissing GIFs.
+ - /femdom: Sends Random Femdom source Images.
+ - /cuddle: Sends Random Cuddle GIFs.
+ - /erok: Sends Random Ero-Kitsune source Images.
+ - /foxgirl: Sends Random FoxGirl source Images.
+ - /titsgif: Sends Random Tits GIFs.
+ - /ero: Sends Random Ero source Images.
+ - /smug: Sends Random Smug GIFs.
+ - /baka: Sends Random Baka Shout GIFs.
+ - /dva: Sends Random D.VA source Images.
+"""
 
 __mod_name__ = "Nsғᴡ"
