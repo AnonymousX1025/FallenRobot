9a419d15c4fe21e508ae19e342694d1fea4f0823
Anonymous <rahulking25102006@gmail.com>
2022-03-22 12:22:40 +0530

    Update functions.py

--- FallenRobot/utils/functions.py
+++ FallenRobot/utils/functions.py
@@ -1,1 +1,11 @@
-from asyncio import gather<br/>from io import BytesIO<div></div>from FallenRobot import aiohttpsession as aiosession<div></div><br/>async def make_carbon(code):<br/>    url = "https://carbonara.vercel.app/api/cook"<br/>    async with aiosession.post(url, json={"code": code}) as resp:<br/>        image = BytesIO(await resp.read())<br/>    image.name = "carbon.png"<br/>    return image<br/>
+from asyncio import gather
+from io import BytesIO
+from FallenRobot import aiohttpsession as aiosession
+
+
+async def make_carbon(code):
+    url = "https://carbonara.vercel.app/api/cook"
+    async with aiosession.post(url, json={"code": code}) as resp:
+        image = BytesIO(await resp.read())
+    image.name = "carbon.png"
+    return image
