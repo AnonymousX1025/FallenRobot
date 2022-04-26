54a4b534bd84d41e0474c8ef95d75055b06cf43d
Affectionate <bsantanu990@gmail.com>
2022-04-26 17:47:19 +0530

    Update __main__.py

--- FallenRobot/__main__.py
+++ FallenRobot/__main__.py
@@ -98,7 +98,7 @@ buttons = [
             text="✗ sᴜᴘᴘᴏʀᴛ ✗", url="https://t.me/DevilsHeavenMF"
         ),
         InlineKeyboardButton(
-            text="✗ ᴅᴇᴠᴇʟᴏᴘᴇʀ ✗", url="https://t.me/DEVELOPER"
+            text="✗ ᴅᴇᴠᴇʟᴏᴘᴇʀ ✗", url="https://t.me/{DEVELOPER}"
         ),
     ],
 ]
