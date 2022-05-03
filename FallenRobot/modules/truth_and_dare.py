d12bb5e67c3a5f6b29acd6d6c92901de9a100818
ᴀɴᴏɴʏᴍᴏᴜs <rahulking25102006@gmail.com>
2022-05-03 23:50:25 +0530

    Create truth_and_dare.py

---
+++ FallenRobot/modules/truth_and_dare.py
@@ -0,0 +1,31 @@
+import html
+import random
+import FallenRobot.modules.truth_and_dare_string as truth_and_dare_string
+from FallenRobot import dispatcher
+from telegram import ParseMode, Update, Bot
+from FallenRobot.modules.disable import DisableAbleCommandHandler
+from telegram.ext import CallbackContext, run_async
+
+
+def truth(update: Update, context: CallbackContext):
+    args = context.args
+    update.effective_message.reply_text(random.choice(truth_and_dare_string.TRUTH))
+
+
+def dare(update: Update, context: CallbackContext):
+    args = context.args
+    update.effective_message.reply_text(random.choice(truth_and_dare_string.DARE))
+
+
+TRUTH_HANDLER = DisableAbleCommandHandler("truth", truth, run_async=True)
+DARE_HANDLER = DisableAbleCommandHandler("dare", dare, run_async=True)
+
+dispatcher.add_handler(TRUTH_HANDLER)
+dispatcher.add_handler(DARE_HANDLER)
+
+__help__ = """
+*Truth & Dare*
+ ❍ /truth *:* Sends a random truth string.
+ ❍ /dare *:* Sends a random dare string.
+"""
+__mod_name__ = "Tʀᴜᴛʜ-Dᴀʀᴇ"
