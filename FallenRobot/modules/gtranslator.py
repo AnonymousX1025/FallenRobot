11aeac1e2471aa78b48425c0c9532ad12d311096
ᴀɴᴏɴʏᴍᴏᴜs <rahulking25102006@gmail.com>
2022-05-27 23:10:34 +0530

    Update and rename gtranslator.txt to gtranslator.py

--- FallenRobot/modules/gtranslator.py
+++ FallenRobot/modules/gtranslator.py
@@ -1,18 +1,22 @@
+from emoji import UNICODE_EMOJI
+from telegram import  Update, ParseMode 
+from telegram.ext import run_async, CallbackContext
 from gpytranslate import SyncTranslator
-from telegram import ParseMode, Update
-from telegram.ext import CallbackContext
-
 from FallenRobot import dispatcher
 from FallenRobot.modules.disable import DisableAbleCommandHandler
-
 trans = SyncTranslator()
-
-def translate(update: Update, context: CallbackContext) -> None:
-    bot = context.bot
+ 
+def totranslate(update: Update, context: CallbackContext) -> None:
     message = update.effective_message
     reply_msg = message.reply_to_message
     if not reply_msg:
-        message.reply_text("Reply to a message to translate it!")
+        message.reply_text(
+            "Reply to messages or write messages from other languages ​​for translating into the intended language\n\n"
+            "Example: `/tr en-ja` to translate from English to Japanese\n"
+            "Or use: `/tr ja` for automatic detection and translating it into japanese.\n"
+            "See [List of Language Codes](t.me/fateunionupdates/32) for a list of language codes.",
+            parse_mode="markdown",
+            disable_web_page_preview=True)
         return
     if reply_msg.caption:
         to_translate = reply_msg.caption
@@ -30,20 +34,13 @@ def translate(update: Update, context: CallbackContext) -> None:
         source = trans.detect(to_translate)
         dest = "en"
     translation = trans(to_translate,
-                        sourcelang=source, targetlang=dest)
+                              sourcelang=source, targetlang=dest)
     reply = f"<b>Translated from {source} to {dest}</b>:\n" \
         f"<code>{translation.text}</code>"
-
-    bot.send_message(text=reply, chat_id=message.chat.id, parse_mode=ParseMode.HTML)
-
-
-def languages(update: Update, context: CallbackContext) -> None:
-    message = update.effective_message
-    bot = context.bot
-    bot.send_message(
-        text="Click [here](https://t.me/) to see the list of supported language codes!",
-        chat_id=message.chat.id, disable_web_page_preview=True, parse_mode=ParseMode.MARKDOWN)
-
+ 
+    message.reply_text(reply, parse_mode=ParseMode.HTML)
+ 
+ 
 __help__ = """
  ❍ /tr or /tl (language code) as reply to a long message
 *Example:* 
@@ -58,13 +55,11 @@ ml,mn,mr,ms,mt,my,ne,nl,no,ny,pa,pl,ps,pt,ro,ru,sd,si,sk,sl,
 sm,sn,so,sq,sr,st,su,sv,sw,ta,te,tg,th,tl,tr,uk,ur,uz,
 vi,xh,yi,yo,zh,zh_CN,zh_TW,zu`
 """
-
-TRANSLATE_HANDLER = DisableAbleCommandHandler(["tr", "tl"], totranslate)
-TRANSLATE_LANG_HANDLER = DisableAbleCommandHandler(["lang", "languages"], languages)
-
+__mod_name__ = "Tʀᴀɴsʟᴀᴛᴏʀ"
+ 
+TRANSLATE_HANDLER = DisableAbleCommandHandler(["tr", "tl"], totranslate, run_async=True)
+ 
 dispatcher.add_handler(TRANSLATE_HANDLER)
-dispatcher.add_handler(TRANSLATE_LANG_HANDLER)
-
-__mod_name__ = "G-ᴛʀᴀɴs"
-__command_list__ = ["tr", "tl", "lang", "languages"]
-__handlers__ = [TRANSLATE_HANDLER, TRANSLATE_LANG_HANDLER]
+ 
+__command_list__ = ["tr", "tl"]
+__handlers__ = [TRANSLATE_HANDLER]
