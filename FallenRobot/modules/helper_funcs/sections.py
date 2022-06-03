d8113ad9a9d820fb9773dba6cc11a25dc01963e5
ᴀɴᴏɴʏᴍᴏᴜs <rahulking25102006@gmail.com>
2022-06-04 00:51:02 +0530

    Create sections.py

---
+++ FallenRobot/modules/helper_funcs/sections.py
@@ -0,0 +1,24 @@
+n = "\n"
+w = " "
+
+bold = lambda x: f"**{x}:** "
+bold_ul = lambda x: f"**--{x}:**-- "
+
+mono = lambda x: f"`{x}`{n}"
+
+
+def section(
+        title: str,
+        body: dict,
+        indent: int = 2,
+        underline: bool = False,
+) -> str:
+    text = (bold_ul(title) + n) if underline else bold(title) + n
+
+    for key, value in body.items():
+        text += (
+                indent * w
+                + bold(key)
+                + ((value[0] + n) if isinstance(value, list) else mono(value))
+        )
+    return text
