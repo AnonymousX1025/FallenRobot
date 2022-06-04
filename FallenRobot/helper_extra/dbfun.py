15239dc048239564c7056fa6cdc9f63171df9d87
ᴀɴᴏɴʏᴍᴏᴜs <rahulking25102006@gmail.com>
2022-06-04 20:58:26 +0530

    Update dbfun.py

--- FallenRobot/helper_extra/dbfun.py
+++ FallenRobot/helper_extra/dbfun.py
@@ -76,6 +76,23 @@ async def update_karma(chat_id: int, name: str, karma: dict):
 
 
 
+async def is_karma_on(chat_id: int) -> bool:
+    chat = await karmadb.find_one({"chat_id_toggle": chat_id})
+    return not chat
+
+
+async def karma_on(chat_id: int):
+    is_karma = await is_karma_on(chat_id)
+    if is_karma:
+        return
+    return await karmadb.delete_one({"chat_id_toggle": chat_id})
+
+
+async def karma_off(chat_id: int):
+    is_karma = await is_karma_on(chat_id)
+    if not is_karma:
+        return
+    return await karmadb.insert_one({"chat_id_toggle": chat_id})
 
 
 async def int_to_alpha(user_id: int) -> str:
