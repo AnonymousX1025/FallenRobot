7b9a17e09772341dbf307cf7fe8ac33865331afb
ᴀɴᴏɴʏᴍᴏᴜs <rahulking25102006@gmail.com>
2022-06-04 23:28:49 +0530

    Update dbfun.py

--- FallenRobot/helper_extra/dbfun.py
+++ FallenRobot/helper_extra/dbfun.py
@@ -7,8 +7,6 @@ client = MongoClient(MONGO_DB_URI)
 db = client["FallenRobot"]
 
 coupledb = db.couple
-karmadb = db.karma
-
 
 
 
@@ -35,64 +33,6 @@ async def save_couple(chat_id: int, date: str, couple: dict):
     lovers[date] = couple
     coupledb.update_one({"chat_id": chat_id}, {"$set": {"couple": lovers}}, upsert=True)
 
-
-
-
-
-async def get_karmas_count() -> dict:
-    chats = karmadb.find({"chat_id": {"$lt": 0}})
-    if not chats:
-        return {}
-    chats_count = 0
-    karmas_count = 0
-    for chat in await chats.to_list(length=1000000):
-        for i in chat["karma"]:
-            karmas_count += chat["karma"][i]["karma"]
-        chats_count += 1
-    return {"chats_count": chats_count, "karmas_count": karmas_count}
-
-
-async def get_karmas(chat_id: int) -> Dict[str, int]:
-    karma = karmadb.find_one({"chat_id": chat_id})
-    if karma:
-        karma = karma["karma"]
-    else:
-        karma = {}
-    return karma
-
-
-async def get_karma(chat_id: int, name: str) -> Union[bool, dict]:
-    name = name.lower().strip()
-    karmas = await get_karmas(chat_id)
-    if name in karmas:
-        return karmas[name]
-
-
-async def update_karma(chat_id: int, name: str, karma: dict):
-    name = name.lower().strip()
-    karmas = await get_karmas(chat_id)
-    karmas[name] = karma
-    karmadb.update_one({"chat_id": chat_id}, {"$set": {"karma": karmas}}, upsert=True)
-
-
-
-async def is_karma_on(chat_id: int) -> bool:
-    chat = await karmadb.find_one({"chat_id_toggle": chat_id})
-    return not chat
-
-
-async def karma_on(chat_id: int):
-    is_karma = await is_karma_on(chat_id)
-    if is_karma:
-        return
-    return await karmadb.delete_one({"chat_id_toggle": chat_id})
-
-
-async def karma_off(chat_id: int):
-    is_karma = await is_karma_on(chat_id)
-    if not is_karma:
-        return
-    return await karmadb.insert_one({"chat_id_toggle": chat_id})
 
 
 async def int_to_alpha(user_id: int) -> str:
