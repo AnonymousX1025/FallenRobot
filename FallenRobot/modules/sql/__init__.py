634f443d6854ebee2615f81236e91459c6c405f3
ᴀɴᴏɴʏᴍᴏᴜs <rahulking25102006@gmail.com>
2022-06-05 01:28:58 +0530

    Update __init__.py

--- FallenRobot/modules/sql/__init__.py
+++ FallenRobot/modules/sql/__init__.py
@@ -1,15 +1,24 @@
-from FallenRobot import DB_URI
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy.orm import sessionmaker, scoped_session
 
+from FallenRobot import DB_URI, LOGGER as log
+
+if DB_URI and DB_URI.startswith("postgres://"):
+    DB_URI = DB_URI.replace("postgres://", "postgresql://", 1)
 
 def start() -> scoped_session:
     engine = create_engine(DB_URI, client_encoding="utf8")
+    log.info("[PostgreSQL] Connecting to database......")
     BASE.metadata.bind = engine
     BASE.metadata.create_all(engine)
     return scoped_session(sessionmaker(bind=engine, autoflush=False))
 
-
 BASE = declarative_base()
-SESSION = start()
+try:
+    SESSION = start()
+except Exception as e:
+    log.exception(f'[PostgreSQL] Failed to connect due to {e}')
+    exit()
+   
+log.info("[PostgreSQL] Connection successful, session started.")
