19fe2edab5d7ee8bf33e44c0d4d4f350d9fcde13
ᴀɴᴏɴʏᴍᴏᴜs <rahulking25102006@gmail.com>
2022-06-12 11:35:15 +0530

    Update events.py

--- FallenRobot/events.py
+++ FallenRobot/events.py
@@ -1,5 +1,4 @@
 import inspect
-import glob
 import logging
 import sys
 import re
@@ -21,7 +20,7 @@ def register(**args):
     """Registers a new message."""
     pattern = args.get("pattern", None)
 
-    r_pattern = r"^[/!]"
+    r_pattern = r"^[/!.]"
 
     if pattern is not None and not pattern.startswith("(?i)"):
         args["pattern"] = "(?i)" + pattern
@@ -177,41 +176,3 @@ def fallenrobot(**args):
         if not ignore_unsafe:
             args["pattern"] = args["pattern"].replace("^.", unsafe_pattern, 1)
 
-
-def load_module(shortname):
-    if shortname.startswith("__"):
-        pass
-    elif shortname.endswith("_"):
-        import importlib
-        import FallenRobot.events
-
-        path = Path(f"FallenRobot/modules/{shortname}.py")
-        name = "FallenRobot.modules.{}".format(shortname)
-        spec = importlib.util.spec_from_file_location(name, path)
-        mod = importlib.util.module_from_spec(spec)
-        spec.loader.exec_module(mod)
-        print("Successfully imported " + shortname)
-    else:
-        import importlib
-        import FallenRobot.events
-
-        path = Path(f"FallenRobot/modules/{shortname}.py")
-        name = "FallenRobot.modules.{}".format(shortname)
-        spec = importlib.util.spec_from_file_location(name, path)
-        mod = importlib.util.module_from_spec(spec)
-        mod.register = register
-        mod.fallenrobot = fallenrobot
-        mod.tbot = telethn
-        mod.logger = logging.getLogger(shortname)
-        spec.loader.exec_module(mod)
-        sys.modules["FallenRobot.modules." + shortname] = mod
-        print("Successfully imported " + shortname)
-
-
-path = "FallenRobot/modules/*.py"
-files = glob.glob(path)
-for name in files:
-    with open(name) as f:
-        path1 = Path(f.name)
-        shortname = path1.stem
-        load_module(shortname.replace(".py", ""))
