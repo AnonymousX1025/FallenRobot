cbdc7803d967f5cce78a8a27f04dae752c1ef41f
ᴀɴᴏɴʏᴍᴏᴜs <rahulking25102006@gmail.com>
2022-06-19 02:06:32 +0530

    Update dbfun.py

--- FallenRobot/helper_extra/dbfun.py
+++ FallenRobot/helper_extra/dbfun.py
@@ -7,7 +7,6 @@ client = MongoClient(MONGO_DB_URI)
 db = client["FallenRobot"]
 
 coupledb = db.couple
-usersdb = db.users
 
 
 async def _get_lovers(chat_id: int):
@@ -50,32 +49,3 @@ async def alpha_to_int(user_id_alphabet: str) -> int:
         user_id += str(index)
     user_id = int(user_id)
     return user_id
-
-
-async def is_afk(user_id: int) -> bool:
-    user = await usersdb.find_one({"user_id": user_id})
-    if not user:
-        return False, {}
-    return True, user["reason"]
-
-
-async def add_afk(user_id: int, mode):
-    await usersdb.update_one(
-        {"user_id": user_id}, {"$set": {"reason": mode}}, upsert=True
-    )
-
-
-async def remove_afk(user_id: int):
-    user = await usersdb.find_one({"user_id": user_id})
-    if user:
-        return await usersdb.delete_one({"user_id": user_id})
-
-
-async def get_afk_users() -> list:
-    users = usersdb.find({"user_id": {"$gt": 0}})
-    if not users:
-        return []
-    users_list = []
-    for user in await users.to_list(length=1000000000):
-        users_list.append(user)
-    return users_list
