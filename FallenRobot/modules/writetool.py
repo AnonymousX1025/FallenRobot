216261c39d40d2853c916807420ffef8ec7b5431
ᴀɴᴏɴʏᴍᴏᴜs <rahulking25102006@gmail.com>
2022-06-28 13:04:50 +0530

    Better hamdwriting
    Time to complete your assignments

--- FallenRobot/modules/writetool.py
+++ FallenRobot/modules/writetool.py
@@ -1,25 +1,69 @@
+import requests
+from requests import get
 from pyrogram import filters
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 
-from FallenRobot import pbot
+from FallenRobot import pbot as fallen, dispatcher, SUPPORT_CHAT
 
 
-@pbot.on_message(filters.command("write"))
-async def handwriting(_, message):
-    if len(message.command) < 2:
-        return await message.reply_text("» ɢɪᴠᴇ sᴏᴍᴇ ᴛᴇxᴛ ᴛᴏ ᴡʀɪᴛᴇ ɪᴛ ᴏɴ ᴍʏ ᴄᴏᴩʏ...")
-    m = await message.reply_text("» ᴡᴀɪᴛ ᴀ sᴇᴄ, ʟᴇᴛ ᴍᴇ ᴡʀɪᴛᴇ ᴛʜᴀᴛ ᴛᴇxᴛ...")
-    name = (
-        message.text.split(None, 1)[1]
-        if len(message.command) < 3
-        else message.text.split(None, 1)[1].replace(" ", "%20")
-    )
-    hand = "https://apis.xditya.me/write?text=" + name
-    await m.edit("» ᴜᴩʟᴏᴀᴅɪɴɢ...")
-    await pbot.send_chat_action(message.chat.id, "upload_photo")
-    await message.reply_photo(
-        hand, caption="ᴡʀɪᴛᴛᴇɴ ᴡɪᴛʜ 🖊 ʙʏ [ғᴀʟʟᴇɴ](t.me/FallenXRobot)"
-    )
+@fallen.on_message(filters.command("write"))
+async def handwrite(_, message: Message):
+  if not message.reply_to_message:  
+    text = message.text.split(None, 1)[1]
+    m = await fallen.send_message(message.chat.id, "**ᴩʟᴇᴀsᴇ ᴡᴀɪᴛ...**\n\nʟᴇᴍᴍᴇ ᴡʀɪᴛᴇ ɪᴛ ᴏɴ ᴍʏ ᴄᴏᴩʏ...")
+    API = "https://single-developers.up.railway.app/write"
+    body = {     
+     "text":f"{text}"     
+    }
+    req = requests.post(API, headers={'Content-Type': 'application/json'}, json=body)
+    photo = req.history[1].url
+    caption = f"""
+sᴜᴄᴄᴇssғᴜʟʟʏ ᴡʀɪᴛᴛᴇɴ ᴛᴇxᴛ 💘
+
+✨ **ᴡʀɪᴛᴛᴇɴ ʙʏ :** [{dispatcher.bot.first_name}](https://t.me/{dispatcher.bot.username})
+🥀 **ʀᴇǫᴜᴇsᴛᴇᴅ ʙʏ :** {message.from_user.mention}
+❄ **ʟɪɴᴋ :** `{photo}`
+"""    
     await m.delete()
+    await fallen.send_photo(message.chat.id, photo=photo, caption=caption,
+                 reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "• ᴛᴇʟᴇɢʀᴀᴩʜ •", url=f"{photo}"
+                    )
+                ]
+            ]
+          ),
+    )
+    lol = message.reply_to_message.text
+    m = await fallen.send_message(message.chat.id, "**ᴩʟᴇᴀsᴇ ᴡᴀɪᴛ...**\n\nʟᴇᴍᴍᴇ ᴡʀɪᴛᴇ ɪᴛ ᴏɴ ᴍʏ ᴄᴏᴩʏ...")
+    API = "https://single-developers.up.railway.app/write"
+    body = {     
+     "text":f"{lol}"     
+    }
+    req = requests.post(API, headers={'Content-Type': 'application/json'}, json=body)
+    photo = req.history[1].url
+    caption = f"""
+sᴜᴄᴄᴇssғᴜʟʟʏ ᴡʀɪᴛᴛᴇɴ ᴛᴇxᴛ 💘
+
+✨ **ᴡʀɪᴛᴛᴇɴ ʙʏ :** [{dispatcher.bot.first_name}](https://t.me/{dispatcher.bot.username})
+🥀 **ʀᴇǫᴜᴇsᴛᴇᴅ ʙʏ :** {message.from_user.mention}
+❄ **ʟɪɴᴋ :** `{photo}`
+"""    
+    await m.delete()
+    await fallen.send_photo(message.chat.id, photo=photo, caption=caption,
+                 reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "• ᴛᴇʟᴇɢʀᴀᴩʜ •", url=f"{photo}"
+                    )
+                ]
+            ]
+          ),
+    )
+
 
 
 __mod_name__ = "WʀɪᴛᴇTᴏᴏʟ"
