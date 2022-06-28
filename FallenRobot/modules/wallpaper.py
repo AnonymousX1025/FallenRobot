84e7ad41b2abad2869b05d2a67ae202241ebc3af
ᴀɴᴏɴʏᴍᴏᴜs <rahulking25102006@gmail.com>
2022-06-28 11:54:09 +0530

    Removed alphacoders wall api

--- FallenRobot/modules/wallpaper.py
+++ FallenRobot/modules/wallpaper.py
@@ -1,59 +1,63 @@
-from random import randint
+import os
+import io
+import requests
+from requests import get
+from pyrogram.types import Message
+from FallenRobot import pbot, dispatcher, SUPPORT_CHAT
+from bs4 import *
+from pyrogram import filters	
+from PIL import Image
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-import requests as r
-from FallenRobot import SUPPORT_CHAT, WALL_API, dispatcher
-from FallenRobot.modules.disable import DisableAbleCommandHandler
-from telegram import Update
-from telegram.ext import CallbackContext, run_async
-
-# Wallpapers module by @TheRealPhoenix using wall.alphacoders.com
-
-
-@run_async
-def wall(update: Update, context: CallbackContext):
-    chat_id = update.effective_chat.id
-    msg = update.effective_message
-    args = context.args
-    msg_id = update.effective_message.message_id
-    bot = context.bot
-    query = " ".join(args)
-    if not query:
-        msg.reply_text("Please enter a query!")
-        return
+def get_text(message: Message) -> [None, str]:
+    text_to_return = message.text
+    if message.text is None:
+        return None
+    if " " in text_to_return:
+        try:
+            return message.text.split(None, 1)[1]
+        except IndexError:
+            return None
     else:
-        caption = query
-        term = query.replace(" ", "%20")
-        json_rep = r.get(
-            f"https://wall.alphacoders.com/api2.0/get.php?auth={WALL_API}&method=search&term={term}"
-        ).json()
-        if not json_rep.get("success"):
-            msg.reply_text(f"An error occurred! Report this @{SUPPORT_CHAT}")
-        else:
-            wallpapers = json_rep.get("wallpapers")
-            if not wallpapers:
-                msg.reply_text("No results found! Refine your search.")
-                return
-            else:
-                index = randint(0, len(wallpapers) - 1)  # Choose random index
-                wallpaper = wallpapers[index]
-                wallpaper = wallpaper.get("url_image")
-                wallpaper = wallpaper.replace("\\", "")
-                bot.send_photo(
-                    chat_id,
-                    photo=wallpaper,
-                    caption="Preview",
-                    reply_to_message_id=msg_id,
-                    timeout=60,
-                )
-                bot.send_document(
-                    chat_id,
-                    document=wallpaper,
-                    filename="wallpaper",
-                    caption=caption,
-                    reply_to_message_id=msg_id,
-                    timeout=60,
-                )
+        return None
 
 
-WALLPAPER_HANDLER = DisableAbleCommandHandler("wall", wall)
-dispatcher.add_handler(WALLPAPER_HANDLER)
+
+@pbot.on_message(filters.command(["wall", "wallpaper"]))
+async def wall(client, message):      
+ quew = get_text(message)
+ if not quew:
+     await client.send_message(message.chat.id, "😶 **ᴩʟᴇᴀsᴇ ɢɪᴠᴇ sᴏᴍᴇ ᴛᴇxᴛ ᴛᴏ sᴇᴀʀᴄʜ ғᴏʀ ᴡᴀʟʟᴩᴀᴩᴇʀ !**")
+     return
+ m = await client.send_message(message.chat.id, "⚙️ **sᴇᴀʀᴄʜɪɴɢ ғᴏʀ ᴡᴀʟʟᴩᴀᴩᴇʀ...**")
+ try:
+    text = get_text(message)
+    LOGO_API = f"https://single-developers.up.railway.app/wallpaper?search={text}"
+    randc = (LOGO_API)
+    murl = requests.get(f"https://single-developers.up.railway.app/wallpaper?search={text}").history[1].url
+    img = Image.open(io.BytesIO(requests.get(randc).content))
+    fname = "fallenrobot.png"
+    img.save(fname, "png")
+    caption = f"""
+💘 ᴡᴀʟʟᴩᴀᴩᴇʀ ɢᴇɴᴇʀᴀᴛᴇᴅ sᴜᴄᴄᴇssғᴜʟʟʏ 
+
+✨ **ɢᴇɴᴇʀᴀᴛᴇᴅ ʙʏ :** [{dispatcher.bot.first_name}](https://t.me/{dispatcher.bot.username})
+🥀 **ʀᴇǫᴜᴇsᴛᴇᴅ ʙʏ :** {message.from_user.mention}
+❄ **ᴅᴏᴡɴʟᴏᴀᴅ :** `{murl}`
+"""
+    await m.delete()
+    await client.send_photo(message.chat.id, photo=murl, caption=caption,
+                 reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "• ʟɪɴᴋ •", url=f"{murl}"
+                    )
+                ],
+            ]
+          ),
+    )
+    if os.path.exists(fname):
+            os.remove(fname)
+ except Exception as e:
+    await client.send_message(message.chat.id, f"sᴏᴍᴇᴛʜɪɴɢ ᴡᴇɴᴛ ᴡʀᴏɴɢ.\nᴩʟᴇᴀsᴇ ʀᴇᴩᴏʀᴛ ᴛʜɪs ᴀᴛ @{SUPPORT_CHAT}\n\n**ᴇʀʀᴏʀ :** {e}")
