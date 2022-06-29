0e0c4c6dec11ae01c0cf67f47116e9ac2ef5d3f6
AnonymousR1025 <AnonymousR1025@users.noreply.github.com>
2022-06-29 06:19:53 +0000

    Automated code formatting
    Signed-off-by: AnonymousR1025 <102808014+AnonymousR1025@users.noreply.github.com>

--- FallenRobot/modules/wallpaper.py
+++ FallenRobot/modules/wallpaper.py
@@ -5,9 +5,10 @@ from requests import get
 from pyrogram.types import Message
 from FallenRobot import pbot, dispatcher, SUPPORT_CHAT
 from bs4 import *
-from pyrogram import filters	
+from pyrogram import filters
 from PIL import Image
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
 
 def get_text(message: Message) -> [None, str]:
     text_to_return = message.text
@@ -22,42 +23,51 @@ def get_text(message: Message) -> [None, str]:
         return None
 
 
-
 @pbot.on_message(filters.command(["wall", "wallpaper"]))
-async def wall(client, message):      
- quew = get_text(message)
- if not quew:
-     await client.send_message(message.chat.id, "😶 **ᴩʟᴇᴀsᴇ ɢɪᴠᴇ sᴏᴍᴇ ᴛᴇxᴛ ᴛᴏ sᴇᴀʀᴄʜ ғᴏʀ ᴡᴀʟʟᴩᴀᴩᴇʀ !**")
-     return
- m = await client.send_message(message.chat.id, "⚙️ **sᴇᴀʀᴄʜɪɴɢ ғᴏʀ ᴡᴀʟʟᴩᴀᴩᴇʀ...**")
- try:
-    text = get_text(message)
-    LOGO_API = f"https://single-developers.up.railway.app/wallpaper?search={text}"
-    randc = (LOGO_API)
-    murl = requests.get(f"https://single-developers.up.railway.app/wallpaper?search={text}").history[1].url
-    img = Image.open(io.BytesIO(requests.get(randc).content))
-    fname = "fallenrobot.png"
-    img.save(fname, "png")
-    caption = f"""
+async def wall(client, message):
+    quew = get_text(message)
+    if not quew:
+        await client.send_message(
+            message.chat.id, "😶 **ᴩʟᴇᴀsᴇ ɢɪᴠᴇ sᴏᴍᴇ ᴛᴇxᴛ ᴛᴏ sᴇᴀʀᴄʜ ғᴏʀ ᴡᴀʟʟᴩᴀᴩᴇʀ !**"
+        )
+        return
+    m = await client.send_message(message.chat.id, "⚙️ **sᴇᴀʀᴄʜɪɴɢ ғᴏʀ ᴡᴀʟʟᴩᴀᴩᴇʀ...**")
+    try:
+        text = get_text(message)
+        LOGO_API = f"https://single-developers.up.railway.app/wallpaper?search={text}"
+        randc = LOGO_API
+        murl = (
+            requests.get(
+                f"https://single-developers.up.railway.app/wallpaper?search={text}"
+            )
+            .history[1]
+            .url
+        )
+        img = Image.open(io.BytesIO(requests.get(randc).content))
+        fname = "fallenrobot.png"
+        img.save(fname, "png")
+        caption = f"""
 💘 ᴡᴀʟʟᴩᴀᴩᴇʀ ɢᴇɴᴇʀᴀᴛᴇᴅ sᴜᴄᴄᴇssғᴜʟʟʏ 
 
 ✨ **ɢᴇɴᴇʀᴀᴛᴇᴅ ʙʏ :** [{dispatcher.bot.first_name}](https://t.me/{dispatcher.bot.username})
 🥀 **ʀᴇǫᴜᴇsᴛᴇᴅ ʙʏ :** {message.from_user.mention}
 ❄ **ᴅᴏᴡɴʟᴏᴀᴅ :** `{murl}`
 """
-    await m.delete()
-    await client.send_photo(message.chat.id, photo=murl, caption=caption,
-                 reply_markup=InlineKeyboardMarkup(
-            [
+        await m.delete()
+        await client.send_photo(
+            message.chat.id,
+            photo=murl,
+            caption=caption,
+            reply_markup=InlineKeyboardMarkup(
                 [
-                    InlineKeyboardButton(
-                        "• ʟɪɴᴋ •", url=f"{murl}"
-                    )
-                ],
-            ]
-          ),
-    )
-    if os.path.exists(fname):
+                    [InlineKeyboardButton("• ʟɪɴᴋ •", url=f"{murl}")],
+                ]
+            ),
+        )
+        if os.path.exists(fname):
             os.remove(fname)
- except Exception as e:
-    await client.send_message(message.chat.id, f"sᴏᴍᴇᴛʜɪɴɢ ᴡᴇɴᴛ ᴡʀᴏɴɢ.\nᴩʟᴇᴀsᴇ ʀᴇᴩᴏʀᴛ ᴛʜɪs ᴀᴛ @{SUPPORT_CHAT}\n\n**ᴇʀʀᴏʀ :** {e}")
+    except Exception as e:
+        await client.send_message(
+            message.chat.id,
+            f"sᴏᴍᴇᴛʜɪɴɢ ᴡᴇɴᴛ ᴡʀᴏɴɢ.\nᴩʟᴇᴀsᴇ ʀᴇᴩᴏʀᴛ ᴛʜɪs ᴀᴛ @{SUPPORT_CHAT}\n\n**ᴇʀʀᴏʀ :** {e}",
+        )
