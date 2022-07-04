50c152aef53544be488de0b58049017d5b539cdb
ᴀɴᴏɴʏᴍᴏᴜs <rahulking25102006@gmail.com>
2022-07-04 09:53:42 +0530

    Update paste.py

--- FallenRobot/modules/paste.py
+++ FallenRobot/modules/paste.py
@@ -30,7 +30,7 @@ async def isPreviewUp(preview: str) -> bool:
     return False
 
 
-@app.on_message(filters.command("paste") & ~filters.edited)
+@app.on_message(filters.command("paste"))
 @capture_err
 async def paste_func(_, message):
     if not message.reply_to_message:
