7379a62fc4c20ff816d31c0f46289c4d725c9f8e
ᴀɴᴏɴʏᴍᴏᴜs <rahulking25102006@gmail.com>
2022-07-09 00:32:01 +0530

    Create adminperms.py

---
+++ FallenRobot/utils/adminperms.py
@@ -0,0 +1,28 @@
+import asyncio
+from pyrogram import filters
+from pyrogram.types import ChatPermissions, Message
+from FallenRobot import pbot as app
+
+
+async def member_permissions(chat_id: int, user_id: int):
+    perms = []
+    member = await app.get_chat_member(chat_id, user_id)
+    if member.can_post_messages:
+        perms.append("can_post_messages")
+    if member.can_edit_messages:
+        perms.append("can_edit_messages")
+    if member.can_delete_messages:
+        perms.append("can_delete_messages")
+    if member.can_restrict_members:
+        perms.append("can_restrict_members")
+    if member.can_promote_members:
+        perms.append("can_promote_members")
+    if member.can_change_info:
+        perms.append("can_change_info")
+    if member.can_invite_users:
+        perms.append("can_invite_users")
+    if member.can_pin_messages:
+        perms.append("can_pin_messages")
+    if member.can_manage_voice_chats:
+        perms.append("can_manage_voice_chats")
+    return perms
