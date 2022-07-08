9647aedc94231d25f474f1d61e3c44cb8630e78c
ᴀɴᴏɴʏᴍᴏᴜs <rahulking25102006@gmail.com>
2022-07-08 22:08:48 +0530

    Update afk_mongo.py

--- FallenRobot/helper_extra/afk_mongo.py
+++ FallenRobot/helper_extra/afk_mongo.py
@@ -4,22 +4,22 @@ usersdb = db.users
 
 
 async def is_afk(user_id: int) -> bool:
-    user = await usersdb.find_one({"user_id": user_id})
+    user = usersdb.find_one({"user_id": user_id})
     if not user:
         return False, {}
     return True, user["reason"]
 
 
 async def add_afk(user_id: int, mode):
-    await usersdb.update_one(
+    usersdb.update_one(
         {"user_id": user_id}, {"$set": {"reason": mode}}, upsert=True
     )
 
 
 async def remove_afk(user_id: int):
-    user = await usersdb.find_one({"user_id": user_id})
+    user = usersdb.find_one({"user_id": user_id})
     if user:
-        return await usersdb.delete_one({"user_id": user_id})
+        return usersdb.delete_one({"user_id": user_id})
 
 
 async def get_afk_users() -> list:
@@ -27,6 +27,6 @@ async def get_afk_users() -> list:
     if not users:
         return []
     users_list = []
-    for user in await users.to_list(length=1000000000):
+    for user in users.to_list(length=1000000000):
         users_list.append(user)
     return users_list
