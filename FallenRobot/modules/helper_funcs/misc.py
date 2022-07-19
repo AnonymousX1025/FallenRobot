6431e420cd3880733024d44960f9a4fbfbfad485
ᴀɴᴏɴʏᴍᴏᴜs <rahulking25102006@gmail.com>
2022-07-19 10:45:17 +0530

    Update misc.py

--- FallenRobot/modules/helper_funcs/misc.py
+++ FallenRobot/modules/helper_funcs/misc.py
@@ -80,17 +80,17 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
     if calc in [1, 2]:
         pairs.append((modules[-1],))
 
-    max_num_pages = ceil(len(pairs) / 6)
+    max_num_pages = ceil(len(pairs) / 4)
     modulo_page = page_n % max_num_pages
 
     # can only have a certain amount of buttons side by side
     if len(pairs) > 3:
-        pairs = pairs[modulo_page * 6 : 6 * (modulo_page + 1)] + [
+        pairs = pairs[modulo_page * 4 : 4 * (modulo_page + 1)] + [
             (
                 EqInlineKeyboardButton(
                     "◁", callback_data="{}_prev({})".format(prefix, modulo_page)
                 ),
-                EqInlineKeyboardButton("• ʙᴀᴄᴋ •", callback_data="fallen_back"),
+                EqInlineKeyboardButton("• ʜᴏᴍᴇ •", callback_data="fallen_back"),
                 EqInlineKeyboardButton(
                     "▷", callback_data="{}_next({})".format(prefix, modulo_page)
                 ),
