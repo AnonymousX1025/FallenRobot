f48c9fd625d6418de8940206463ff97964074eb2
ᴀɴᴏɴʏᴍᴏᴜs <rahulking25102006@gmail.com>
2022-07-19 11:26:33 +0530

    Update __main__.py

--- FallenRobot/__main__.py
+++ FallenRobot/__main__.py
@@ -227,6 +227,7 @@ def start(update: Update, context: CallbackContext):
 
         else:
             first_name = update.effective_user.first_name
+            update.effective_message.reply_sticker("CAACAgUAAxkBAAJYsmLWRvm70cE-mmxSNCovEf4v1ueJAAIcCAACbMK4VuL4EmZEkq8WKQQ")
             update.effective_message.reply_text(
                     PM_START_TEXT.format(
                     escape_markdown(first_name), dispatcher.bot.first_name
