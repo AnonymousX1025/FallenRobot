25bcf8082ad1db845d64445b3e2e546ecb60c59c
AnonymousR1025 <AnonymousR1025@users.noreply.github.com>
2022-08-04 03:54:00 +0000

    Automated code formatting
    Signed-off-by: AnonymousR1025 <102808014+AnonymousR1025@users.noreply.github.com>

--- FallenRobot/modules/writetool.py
+++ FallenRobot/modules/writetool.py
@@ -8,63 +8,60 @@ from FallenRobot import pbot as fallen, BOT_NAME, BOT_USERNAME, SUPPORT_CHAT
 
 @fallen.on_message(filters.command("write"))
 async def handwrite(_, message: Message):
-  if not message.reply_to_message:  
-    text = message.text.split(None, 1)[1]
-    m = await fallen.send_message(message.chat.id, "`Please wait...,\n\nWriting your text...`")
-    API = "https://api.sdbots.tk/write"
-    body = {     
-     "text":f"{text}"     
-    }
-    req = requests.post(API, headers={'Content-Type': 'application/json'}, json=body)
-    photo = req.history[1].url
-    caption = f"""
+    if not message.reply_to_message:
+        text = message.text.split(None, 1)[1]
+        m = await fallen.send_message(
+            message.chat.id, "`Please wait...,\n\nWriting your text...`"
+        )
+        API = "https://api.sdbots.tk/write"
+        body = {"text": f"{text}"}
+        req = requests.post(
+            API, headers={"Content-Type": "application/json"}, json=body
+        )
+        photo = req.history[1].url
+        caption = f"""
 Successfully Written Text 💘
 
 ✨ **Written By :** [{BOT_NAME}](https://t.me/{BOT_USERNAME})
 🥀 **Requested by :** {message.from_user.mention}
 ❄ **Link :** `{photo}`
-"""    
-    await m.delete()
-    await fallen.send_photo(message.chat.id, photo=photo, caption=caption,
-                 reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton(
-                        "• ᴛᴇʟᴇɢʀᴀᴩʜ •", url=f"{photo}"
-                    )
-                ]
-            ]
-          ),
-    )
-  else:
-    lol = message.reply_to_message.text
-    m = await fallen.send_message(message.chat.id, "`Please wait...,\n\nWriting your text...`")
-    API = "https://api.sdbots.tk/write"
-    body = {     
-     "text":f"{lol}"     
-    }
-    req = requests.post(API, headers={'Content-Type': 'application/json'}, json=body)
-    photo = req.history[1].url
-    caption = f"""
+"""
+        await m.delete()
+        await fallen.send_photo(
+            message.chat.id,
+            photo=photo,
+            caption=caption,
+            reply_markup=InlineKeyboardMarkup(
+                [[InlineKeyboardButton("• ᴛᴇʟᴇɢʀᴀᴩʜ •", url=f"{photo}")]]
+            ),
+        )
+    else:
+        lol = message.reply_to_message.text
+        m = await fallen.send_message(
+            message.chat.id, "`Please wait...,\n\nWriting your text...`"
+        )
+        API = "https://api.sdbots.tk/write"
+        body = {"text": f"{lol}"}
+        req = requests.post(
+            API, headers={"Content-Type": "application/json"}, json=body
+        )
+        photo = req.history[1].url
+        caption = f"""
 Successfully Written Text 💘
 
 ✨ **Written By :** [{BOT_NAME}](https://t.me/{BOT_USERNAME})
 🥀 **Requested by :** {message.from_user.mention}
 ❄ **Link :** `{photo}`
-"""    
-    await m.delete()
-    await fallen.send_photo(message.chat.id, photo=photo, caption=caption,
-                 reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton(
-                        "• ᴛᴇʟᴇɢʀᴀᴩʜ •", url=f"{photo}"
-                    )
-                ]
-            ]
-          ),
-    )
-
+"""
+        await m.delete()
+        await fallen.send_photo(
+            message.chat.id,
+            photo=photo,
+            caption=caption,
+            reply_markup=InlineKeyboardMarkup(
+                [[InlineKeyboardButton("• ᴛᴇʟᴇɢʀᴀᴩʜ •", url=f"{photo}")]]
+            ),
+        )
 
 
 __mod_name__ = "WʀɪᴛᴇTᴏᴏʟ"
