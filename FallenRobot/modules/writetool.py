6931bae0e6e58892cdc487619c6929447ee7076d
ᴀɴᴏɴʏᴍᴏᴜs <rahulking25102006@gmail.com>
2022-08-04 09:23:37 +0530

    Update writetool.py

--- FallenRobot/modules/writetool.py
+++ FallenRobot/modules/writetool.py
@@ -3,70 +3,68 @@ from requests import get
 from pyrogram import filters
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 
-from FallenRobot import pbot as fallen, dispatcher, SUPPORT_CHAT
+from FallenRobot import pbot as fallen, BOT_NAME, BOT_USERNAME, SUPPORT_CHAT
 
 
 @fallen.on_message(filters.command("write"))
 async def handwrite(_, message: Message):
-    if not message.reply_to_message:
-        name = (
-            message.text.split(None, 1)[1]
-            if len(message.command) < 3
-            else message.text.split(None, 1)[1].replace(" ", "%20")
-        )
-        m = await fallen.send_message(
-            message.chat.id, "**ᴩʟᴇᴀsᴇ ᴡᴀɪᴛ...**\n\nʟᴇᴍᴍᴇ ᴡʀɪᴛᴇ ɪᴛ ᴏɴ ᴍʏ ᴄᴏᴩʏ..."
-        )
-        photo = "https://apis.xditya.me/write?text=" + name
-        caption = f"""
-sᴜᴄᴄᴇssғᴜʟʟʏ ᴡʀɪᴛᴛᴇɴ ᴛᴇxᴛ 💘
+  if not message.reply_to_message:  
+    text = message.text.split(None, 1)[1]
+    m = await fallen.send_message(message.chat.id, "`Please wait...,\n\nWriting your text...`")
+    API = "https://api.sdbots.tk/write"
+    body = {     
+     "text":f"{text}"     
+    }
+    req = requests.post(API, headers={'Content-Type': 'application/json'}, json=body)
+    photo = req.history[1].url
+    caption = f"""
+Successfully Written Text 💘
 
-✨ **ᴡʀɪᴛᴛᴇɴ ʙʏ :** [{dispatcher.bot.first_name}](https://t.me/{dispatcher.bot.username})
-🥀 **ʀᴇǫᴜᴇsᴛᴇᴅ ʙʏ :** {message.from_user.mention}
-"""
-        await fallen.send_photo(
-            message.chat.id,
-            photo=photo,
-            caption=caption,
-            reply_markup=InlineKeyboardMarkup(
+✨ **Written By :** [{BOT_NAME}](https://t.me/{BOT_USERNAME})
+🥀 **Requested by :** {message.from_user.mention}
+❄ **Link :** `{photo}`
+"""    
+    await m.delete()
+    await fallen.send_photo(message.chat.id, photo=photo, caption=caption,
+                 reply_markup=InlineKeyboardMarkup(
+            [
                 [
-                    [
-                        InlineKeyboardButton(
-                            "• sᴜᴩᴩᴏʀᴛ •", url=f"https://t.me/{SUPPORT_CHAT}"
-                        )
-                    ]
+                    InlineKeyboardButton(
+                        "• ᴛᴇʟᴇɢʀᴀᴩʜ •", url=f"{photo}"
+                    )
                 ]
-            ),
-        )
-        await m.delete()
-    else:
-        lol = message.reply_to_message.text
-        name = lol.split(None, 0)[0].replace(" ", "%20")
-        m = await fallen.send_message(
-            message.chat.id, "**ᴩʟᴇᴀsᴇ ᴡᴀɪᴛ...**\n\nʟᴇᴍᴍᴇ ᴡʀɪᴛᴇ ɪᴛ ᴏɴ ᴍʏ ᴄᴏᴩʏ..."
-        )
-        photo = "https://apis.xditya.me/write?text=" + name
-        caption = f"""
-sᴜᴄᴄᴇssғᴜʟʟʏ ᴡʀɪᴛᴛᴇɴ ᴛᴇxᴛ 💘
+            ]
+          ),
+    )
+  else:
+    lol = message.reply_to_message.text
+    m = await fallen.send_message(message.chat.id, "`Please wait...,\n\nWriting your text...`")
+    API = "https://api.sdbots.tk/write"
+    body = {     
+     "text":f"{lol}"     
+    }
+    req = requests.post(API, headers={'Content-Type': 'application/json'}, json=body)
+    photo = req.history[1].url
+    caption = f"""
+Successfully Written Text 💘
 
-✨ **ᴡʀɪᴛᴛᴇɴ ʙʏ :** [{dispatcher.bot.first_name}](https://t.me/{dispatcher.bot.username})
-🥀 **ʀᴇǫᴜᴇsᴛᴇᴅ ʙʏ :** {message.from_user.mention}
-"""
-        await fallen.send_photo(
-            message.chat.id,
-            photo=photo,
-            caption=caption,
-            reply_markup=InlineKeyboardMarkup(
+✨ **Written By :** [{BOT_NAME}](https://t.me/{BOT_USERNAME})
+🥀 **Requested by :** {message.from_user.mention}
+❄ **Link :** `{photo}`
+"""    
+    await m.delete()
+    await fallen.send_photo(message.chat.id, photo=photo, caption=caption,
+                 reply_markup=InlineKeyboardMarkup(
+            [
                 [
-                    [
-                        InlineKeyboardButton(
-                            "• sᴜᴩᴩᴏʀᴛ •", url=f"https://t.me/{SUPPORT_CHAT}"
-                        )
-                    ]
+                    InlineKeyboardButton(
+                        "• ᴛᴇʟᴇɢʀᴀᴩʜ •", url=f"{photo}"
+                    )
                 ]
-            ),
-        )
-        await m.delete()
+            ]
+          ),
+    )
+
 
 
 __mod_name__ = "WʀɪᴛᴇTᴏᴏʟ"
