adb751a8b4bb8ab0b877ee8e1dd276905c3e094c
ᴀɴᴏɴʏᴍᴏᴜs <rahulking25102006@gmail.com>
2022-08-09 09:53:45 +0530

    Update weather.py

--- FallenRobot/modules/weather.py
+++ FallenRobot/modules/weather.py
@@ -31,53 +31,6 @@ async def _(event):
     if event.fwd_from:
         return
 
-    sample_url = (
-        "https://api.openweathermap.org/data/2.5/weather?q={}&APPID={}&units=metric"
-    )
-    input_str = event.pattern_match.group(1)
-    async with aiohttp.ClientSession() as session:
-        response_api_zero = await session.get(
-            sample_url.format(input_str, OPENWEATHERMAP_ID)
-        )
-    response_api = await response_api_zero.json()
-    if response_api["cod"] == 200:
-        country_code = response_api["sys"]["country"]
-        country_time_zone = int(response_api["timezone"])
-        sun_rise_time = int(response_api["sys"]["sunrise"]) + country_time_zone
-        sun_set_time = int(response_api["sys"]["sunset"]) + country_time_zone
-        await event.reply(
-            """**Location**: {}
-**Temperature**: {}°С
-    __minimium__: {}°С
-    __maximum__ : {}°С
-**Humidity**: {}%
-**Wind**: {}m/s
-**Clouds**: {}hpa
-**Sunrise**: {} {}
-**Sunset**: {} {}""".format(
-                input_str,
-                response_api["main"]["temp"],
-                response_api["main"]["temp_min"],
-                response_api["main"]["temp_max"],
-                response_api["main"]["humidity"],
-                response_api["wind"]["speed"],
-                response_api["clouds"]["all"],
-                # response_api["main"]["pressure"],
-                time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime(sun_rise_time)),
-                country_code,
-                time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime(sun_set_time)),
-                country_code,
-            )
-        )
-    else:
-        await event.reply(response_api["message"])
-
-
-@register(pattern="^/wttr (.*)")
-async def _(event):
-    if event.fwd_from:
-        return
-
     sample_url = "https://wttr.in/{}.png"
     # logger.info(sample_url)
     input_str = event.pattern_match.group(1)
@@ -92,9 +45,8 @@ async def _(event):
 __help__ = """
 I can find weather of all cities
 
- ❍ /weather <city>*:* Get weather info in a particular place.
- ❍ /wttr <city>*:* Advanced weather module, usage same as /weather
- ❍ /wttr moon*:* Get the current status of moon
+ ❍ /weather <city>*:* Advanced weather module, usage same as /weather
+ ❍ /weather moon*:* Get the current status of moon
 """
 
 __mod_name__ = "Wᴇᴀᴛʜᴇʀ"
