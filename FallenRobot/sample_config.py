8a02e51dd53716c845adaed67f77e980c50cee38
ᴀɴᴏɴʏᴍᴏᴜs <rahulking25102006@gmail.com>
2022-08-27 15:54:45 +0530

    Update sample_config.py

--- FallenRobot/sample_config.py
+++ FallenRobot/sample_config.py
@@ -1,4 +1,3 @@
-# Create a new config.py or rename this to config.py file in same dir and import, then extend this class.
 import json
 import os
 
@@ -8,35 +7,27 @@ def get_user_list(config, key):
         return json.load(json_file)[key]
 
 
-# Create a new config.py or rename this to config.py file in same dir and import, then extend this class.
+
 class Config(object):
     LOGGER = True
-    # REQUIRED
-    # Login to https://my.telegram.org and fill in these slots with the details given by it
 
-    API_ID = 123456  # integer value, dont use ""
-    API_HASH = "awoo"
-    TOKEN = "BOT_TOKEN"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
-    OWNER_ID = 1356469075  # If you dont know, run the bot and do /id in your private chat with it, also an integer
-    OWNER_USERNAME = "anonymous_was_bot"
-    SUPPORT_CHAT = "DevilsHeavenMF"  # Your own group for support, do not add the @
-    JOIN_LOGGER = (
-        -1001686672798
-    )  # Prints any new group the bot is added to, prints just the name and ID.
-    EVENT_LOGS = (
-        -1001686672798
-    )  # Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
+    API_ID = 
+    API_HASH = ""
+    TOKEN = ""
+    OWNER_ID = 
+    OWNER_USERNAME = ""
+    SUPPORT_CHAT = ""
+    JOIN_LOGGER = ()
+    EVENT_LOGS = ()
 
-    # RECOMMENDED
-    SQLALCHEMY_DATABASE_URI = "something://somewhat:user@hosturl:port/databasename"  # needed for any database modules
+    SQLALCHEMY_DATABASE_URI = ""
+    MONGO_DB_URI = ""
     LOAD = []
-    NO_LOAD = ["rss", "cleaner", "connection", "math"]
+    NO_LOAD = ["rss"]
     WEBHOOK = False
     INFOPIC = True
     URL = None
-    SPAMWATCH_API = ""  # go to support.spamwat.ch to get key
-    SPAMWATCH_SUPPORT_CHAT = "@SpamWatchSupport"
-
+    
     # OPTIONAL
     ##List of id's -  (not usernames) for users which have sudo access to the bot.
     DRAGONS = get_user_list("elevated_users.json", "sudos")
@@ -47,23 +38,29 @@ class Config(object):
     # List of id's (not usernames) for users which WONT be banned/kicked by the bot.
     TIGERS = get_user_list("elevated_users.json", "tigers")
     WOLVES = get_user_list("elevated_users.json", "whitelists")
-    DONATION_LINK = None  # EG, paypal
+
+    DONATION_LINK = ""
     CERT_PATH = None
     PORT = 5000
-    DEL_CMDS = True  # Delete commands that users dont have access to, like delete /ban if a non admin uses it.
+    DEL_CMDS = True
     STRICT_GBAN = True
     WORKERS = (
         8  # Number of subthreads to use. Set as number of threads your processor uses
     )
-    BAN_STICKER = ""  # banhammer marie sticker id, the bot will send this sticker before banning or kicking a user in chat.
-    ALLOW_EXCL = True  # Allow ! commands as well as / (Leave this to true so that blacklist can work)
-    CASH_API_KEY = (
-        "awoo"  # Get your API key from https://www.alphavantage.co/support/#api-key
-    )
-    TIME_API_KEY = "awoo"  # Get your API key from https://timezonedb.com/api
-    WALL_API = (
-        "awoo"  # For wallpapers, get one from https://wall.alphacoders.com/api.php
-    )
+    BAN_STICKER = "CAACAgUAAxkBAAEDafNhq5Z0DegqVzauwSighMw5cPWp8QACVgQAAuUG0FRXfCEuBziNzCIE"
+    ALLOW_EXCL = True 
+    CASH_API_KEY = ""
+    TIME_API_KEY = ""
+    BL_CHATS = []
+    SPAMMERS = None
+    ALLOW_CHATS = True
+    START_IMG = ""
+    HEROKU_API_KEY = None
+    HEROKU_APP_NAME = None
+    TEMP_DOWNLOAD_DIRECTORY = "./"
+    ARQ_API_KEY = "LJMETG-DPHBCX-DGHJCD-TMFIGB-ARQ"
+    ARQ_API_URL = "https://arq.hamker.in"
+    ALLOW_EXCL = None
     BL_CHATS = []  # List of groups that you want blacklisted.
     SPAMMERS = None
 
