2a63ba06f6460b3c0d9c9759520fc4a15a77e5ef
˹ᴧɴσɴʏᴍσᴜs ꭙ˼ <TheAnonymousX1025@gmail.com>
2023-01-07 17:30:41 +0530

    Update writetool.py

--- FallenRobot/modules/writetool.py
+++ FallenRobot/modules/writetool.py
@@ -1,42 +1,52 @@
 import requests
-from pyrogram import filters
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 
-from FallenRobot import BOT_NAME, BOT_USERNAME
-from FallenRobot import pbot as fallen
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ParseMode, Update
+from telegram.ext import CallbackContext
+from telegram.ext.dispatcher import run_async
+
+from FallenRobot import dispatcher, BOT_NAME, BOT_USERNAME
+from FallenRobot.modules.disable import DisableAbleCommandHandler
 
 
-@fallen.on_message(filters.command("write"))
-async def handwrite(_, message: Message):
+@run_async
+def handwrite(update: Update, context: CallbackContext):
+    message = update.effective_message
     if message.reply_to_message:
         text = message.reply_to_message.text
     else:
-        text = message.text.split(None, 1)[1]
-
-        m = await message.reply_text("`Please wait...,\n\nWriting your text...`")
-        API = f"https://api.sdbots.tk/write?text={text}"
-        req = requests.get(API).url
-        caption = f"""
+        text = update.effective_message.text.split(None, 1)[1]
+    m = message.reply_text("Writing the text...")
+    req = requests.get(f"https://api.sdbots.tk/write?text={text}").url
+    message.reply_photo(
+        photo=req,
+        caption=f"""
 Successfully Written Text 💘
 
 ✨ **Written By :** [{BOT_NAME}](https://t.me/{BOT_USERNAME})
-🥀 **Requested by :** {message.from_user.mention}
-❄ **Link :** `{req}`
-"""
-        await m.delete()
-        await message.reply_photo(
-            photo=req,
-            caption=caption,
-            reply_markup=InlineKeyboardMarkup(
-                [[InlineKeyboardButton("• ᴛᴇʟᴇɢʀᴀᴩʜ •", url=req)]]
-            ),
-        )
+🥀 **Requested by :** {update.effective_user.first_name}
+❄ **Link :** `{req}`""",
+        parse_mode=ParseMode.MARKDOWN,
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton("• ᴛᴇʟᴇɢʀᴀᴩʜ •", url=req),
+                ],
+            ]
+        ),
+    )
+    m.delete()
 
-
-__mod_name__ = "WʀɪᴛᴇTᴏᴏʟ"
 
 __help__ = """
  Writes the given text on white page with a pen 🖊
 
 ❍ /write <text> *:* Writes the given text.
- """
+"""
+
+WRITE_HANDLER = DisableAbleCommandHandler("write", handwrite)
+
+dispatcher.add_handler(WRITE_HANDLER)
+
+__mod_name__ = "WʀɪᴛᴇTᴏᴏʟ"
+__command_list__ = ["write"]
+__handlers__ = [WRITE_HANDLER]
