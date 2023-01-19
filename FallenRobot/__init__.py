51daf07095b70c17e7605e49edaedcee64d5d3e5
˹ᴧɴσɴʏᴍσᴜs ꭙ˼ <TheAnonymousX1025@gmail.com>
2023-01-19 13:11:44 +0530

    Update __init__.py

--- FallenRobot/__init__.py
+++ FallenRobot/__init__.py
@@ -17,8 +17,9 @@ logging.basicConfig(
     level=logging.INFO,
 )
 
+logging.getLogger("apscheduler").setLevel(logging.ERROR)
 logging.getLogger("telethon").setLevel(logging.ERROR)
-logging.getLogger("pyrogram").setlevel(logging.ERROR)
+logging.getLogger("pyrogram").setLevel(logging.ERROR)
 LOGGER = logging.getLogger(__name__)
 
 # if version < 3.6, stop bot.
