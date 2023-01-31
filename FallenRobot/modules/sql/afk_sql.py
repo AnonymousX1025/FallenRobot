3d9b0f0b6f7e82923adcb19dd36f14f3c6ec8647
˹ᴧɴσɴʏᴍσᴜs ꭙ˼ <TheAnonymousX1025@gmail.com>
2023-02-01 00:31:08 +0530

    Revert

--- FallenRobot/modules/sql/afk_sql.py
+++ FallenRobot/modules/sql/afk_sql.py
@@ -1,7 +1,6 @@
 import threading
-import time
 
-from sqlalchemy import BigInteger, Boolean, Column, DateTime, UnicodeText
+from sqlalchemy import BigInteger, Boolean, Column, UnicodeText
 
 from FallenRobot.modules.sql import BASE, SESSION
 
@@ -12,13 +11,11 @@ class AFK(BASE):
     user_id = Column(BigInteger, primary_key=True)
     is_afk = Column(Boolean)
     reason = Column(UnicodeText)
-    time = Column(DateTime)
 
-    def __init__(self, user_id: int, reason: str = "", is_afk: bool = True):
+    def __init__(self, user_id, reason="", is_afk=True):
         self.user_id = user_id
         self.reason = reason
         self.is_afk = is_afk
-        self.time = time.time()
 
     def __repr__(self):
         return "afk_status for {}".format(self.user_id)
@@ -49,7 +46,7 @@ def set_afk(user_id, reason=""):
         else:
             curr.is_afk = True
 
-        AFK_USERS[user_id] = {"reason": reason, "time": curr.time}
+        AFK_USERS[user_id] = reason
 
         SESSION.add(curr)
         SESSION.commit()
@@ -87,11 +84,7 @@ def __load_afk_users():
     global AFK_USERS
     try:
         all_afk = SESSION.query(AFK).all()
-        AFK_USERS = {
-            user.user_id: {"reason": user.reason, "time": user.time}
-            for user in all_afk
-            if user.is_afk
-        }
+        AFK_USERS = {user.user_id: user.reason for user in all_afk if user.is_afk}
     finally:
         SESSION.close()
 
