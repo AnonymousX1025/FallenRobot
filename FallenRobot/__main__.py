2188dafad910458a1ccb36618a9389f6900d7410
ᴍʀ. x <114943948+Abishnoi69@users.noreply.github.com>
2023-02-03 18:30:26 +0530

    missing

--- FallenRobot/__main__.py
+++ FallenRobot/__main__.py
@@ -188,6 +188,8 @@ def start(update: Update, context: CallbackContext):
                     ),
                 )
 
+            elif args[0].lower() == "markdownhelp":
+                IMPORTED["Exᴛʀᴀs"].markdown_help_sender(update)
             elif args[0].lower().startswith("stngs_"):
                 match = re.match("stngs_(.*)", args[0].lower())
                 chat = dispatcher.bot.getChat(match.group(1))
