4014eb8929a709452fac76dc9431208a18da0984
˹ᴧɴσɴʏᴍσᴜs ꭙ˼ <TheAnonymousX1025@gmail.com>
2023-04-08 13:19:04 +0530

    Bump python version from 3.10.10 to 3.11.3 and removed karma

--- FallenRobot/utils/admins.py
+++ FallenRobot/utils/admins.py
@@ -6,30 +6,6 @@ from pyrogram.types import Message
 from FallenRobot import DEV_USERS, DRAGONS, pbot
 
 
-def can_change_info(func: Callable) -> Callable:
-    async def non_admin(_, message: Message):
-        if message.from_user.id in DRAGONS:
-            return await func(_, message)
-
-        check = await pbot.get_chat_member(message.chat.id, message.from_user.id)
-        if check.status not in [ChatMemberStatus.OWNER, ChatMemberStatus.ADMINISTRATOR]:
-            return await message.reply_text(
-                "» ʏᴏᴜ'ʀᴇ ɴᴏᴛ ᴀɴ ᴀᴅᴍɪɴ ʙᴀʙʏ, ᴘʟᴇᴀsᴇ sᴛᴀʏ ɪɴ ʏᴏᴜʀ ʟɪᴍɪᴛs."
-            )
-
-        admin = (
-            await pbot.get_chat_member(message.chat.id, message.from_user.id)
-        ).privileges
-        if admin.can_change_info:
-            return await func(_, message)
-        else:
-            return await message.reply_text(
-                "`You don't have permissions to change group info."
-            )
-
-    return non_admin
-
-
 def can_restrict(func: Callable) -> Callable:
     async def non_admin(_, message: Message):
         if message.from_user.id in DEV_USERS:
--- FallenRobot/utils/mongo.py
+++ FallenRobot/utils/mongo.py
@@ -8,7 +8,6 @@ mongo = MongoCli(MONGO_DB_URI)
 db = mongo.FallenRobot
 
 coupledb = db.couple
-karmadb = db.karma
 
 
 async def _get_lovers(chat_id: int):
@@ -36,87 +35,3 @@ async def save_couple(chat_id: int, date: str, couple: dict):
         {"$set": {"couple": lovers}},
         upsert=True,
     )
-
-
-async def get_karmas_count() -> dict:
-    chats_count = 0
-    karmas_count = 0
-    async for chat in karmadb.find({"chat_id": {"$lt": 0}}):
-        for i in chat["karma"]:
-            karma_ = chat["karma"][i]["karma"]
-            if karma_ > 0:
-                karmas_count += karma_
-        chats_count += 1
-    return {"chats_count": chats_count, "karmas_count": karmas_count}
-
-
-async def user_global_karma(user_id) -> int:
-    total_karma = 0
-    async for chat in karmadb.find({"chat_id": {"$lt": 0}}):
-        karma = await get_karma(chat["chat_id"], await int_to_alpha(user_id))
-        if karma and (int(karma["karma"]) > 0):
-            total_karma += int(karma["karma"])
-    return total_karma
-
-
-async def get_karmas(chat_id: int) -> Dict[str, int]:
-    karma = await karmadb.find_one({"chat_id": chat_id})
-    if not karma:
-        return {}
-    return karma["karma"]
-
-
-async def get_karma(chat_id: int, name: str) -> Union[bool, dict]:
-    name = name.lower().strip()
-    karmas = await get_karmas(chat_id)
-    if name in karmas:
-        return karmas[name]
-
-
-async def update_karma(chat_id: int, name: str, karma: dict):
-    name = name.lower().strip()
-    karmas = await get_karmas(chat_id)
-    karmas[name] = karma
-    await karmadb.update_one(
-        {"chat_id": chat_id}, {"$set": {"karma": karmas}}, upsert=True
-    )
-
-
-async def is_karma_on(chat_id: int) -> bool:
-    chat = await karmadb.find_one({"chat_id_toggle": chat_id})
-    if not chat:
-        return True
-    return False
-
-
-async def karma_on(chat_id: int):
-    is_karma = await is_karma_on(chat_id)
-    if is_karma:
-        return
-    return await karmadb.delete_one({"chat_id_toggle": chat_id})
-
-
-async def karma_off(chat_id: int):
-    is_karma = await is_karma_on(chat_id)
-    if not is_karma:
-        return
-    return await karmadb.insert_one({"chat_id_toggle": chat_id})
-
-
-async def int_to_alpha(user_id: int) -> str:
-    alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
-    text = ""
-    user_id = str(user_id)
-    for i in user_id:
-        text += alphabet[int(i)]
-    return text
-
-
-async def alpha_to_int(user_id_alphabet: str) -> int:
-    alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
-    user_id = ""
-    for i in user_id_alphabet:
-        index = alphabet.index(i)
-        user_id += str(index)
-    user_id = int(user_id)
-    return user_id
