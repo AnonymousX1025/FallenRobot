40335b3761cd5de62d262e22bdf9e37e38f4184a
리안 ˹ℵ˼ <128496455+RIAN3755@users.noreply.github.com>
2023-05-01 11:21:32 +0530

    Update __main__.py
    Signed-off-by: 리안 ˹ℵ˼ <128496455+RIAN3755@users.noreply.github.com>

--- FallenRobot/__main__.py
+++ FallenRobot/__main__.py
@@ -205,7 +205,7 @@ def start(update: Update, context: CallbackContext):
         else:
             first_name = update.effective_user.first_name
             update.effective_message.reply_sticker(
-                "CAACAgUAAxkBAAJYsmLWRvm70cE-mmxSNCovEf4v1ueJAAIcCAACbMK4VuL4EmZEkq8WKQQ"
+                "CAACAgUAAx0EaTQW_QADcmRPUzUz3SalMLUYyZOFnWShO_DvAAI3CQACpQ14VpoiXBVOObiqLwQ"
             )
             update.effective_message.reply_text(
                 PM_START_TEXT.format(escape_markdown(first_name), BOT_NAME),
