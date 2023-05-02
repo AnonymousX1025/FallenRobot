3d9058a669d4509e3d20c17c3be6ede64c8a9d8a
리안 ˹ℵ˼ <128496455+RIAN3755@users.noreply.github.com>
2023-05-02 12:13:12 +0530

    Update source.py
    Signed-off-by: 리안 ˹ℵ˼ <128496455+RIAN3755@users.noreply.github.com>

--- FallenRobot/modules/source.py
+++ FallenRobot/modules/source.py
@@ -29,7 +29,7 @@ async def repo(_, message: Message):
                     InlineKeyboardButton("ᴅᴇᴠᴇʟᴏᴘᴇʀ", user_id=OWNER_ID),
                     InlineKeyboardButton(
                         "sᴏᴜʀᴄᴇ",
-                        url="https://github.com/AnonymousX1025/FallenRobot",
+                        url="https://github.com/RIAN3755/HIDDEN-FALLEN",
                     ),
                 ]
             ]
