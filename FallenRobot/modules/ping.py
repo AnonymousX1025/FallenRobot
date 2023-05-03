942bda4df2acb0bff2c70a9ca70a978f2048f6c9
리안 ˹ℵ˼ <128496455+RIAN3755@users.noreply.github.com>
2023-05-03 16:39:10 +0530

    Update ping.py
    Signed-off-by: 리안 ˹ℵ˼ <128496455+RIAN3755@users.noreply.github.com>

--- FallenRobot/modules/ping.py
+++ FallenRobot/modules/ping.py
@@ -46,7 +46,7 @@ def ping(update: Update, context: CallbackContext):
     uptime = get_readable_time((time.time() - StartTime))
 
     message.edit_text(
-        "ɪ ᴀᴍ ᴀʟɪᴠᴇ ʙᴀʙʏ! 🖤\n"
+        "ᴍᴀʀᴀ ɴʜɪ ʜᴜ ʙᴄ! 🖤\n"
         "<b>ᴛɪᴍᴇ ᴛᴀᴋᴇɴ:</b> <code>{}</code>\n"
         "<b>ᴜᴘᴛɪᴍᴇ:</b> <code>{}</code>".format(telegram_ping, uptime),
         parse_mode=ParseMode.HTML,
