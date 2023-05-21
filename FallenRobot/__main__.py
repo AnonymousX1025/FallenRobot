7b83112fae49785c248145c84d0103f750c8e48d
Deva09876 <125013272+Deva09876@users.noreply.github.com>
2023-05-21 23:45:36 +0530

    Update __main__.py
    Signed-off-by: Deva09876 <125013272+Deva09876@users.noreply.github.com>

--- FallenRobot/__main__.py
+++ FallenRobot/__main__.py
@@ -97,7 +97,7 @@ buttons = [
     ],
     [
         InlineKeyboardButton(text="🥀 ᴅᴇᴠᴇʟᴏᴩᴇʀ 🥀", url=f"tg://user?id={OWNER_ID}"),
-        InlineKeyboardButton(text="☁️ sᴏᴜʀᴄᴇ ☁️", callback_data="source_"),
+        InlineKeyboardButton(text="☁️ sᴏᴜʀᴄᴇ ☁️", callback_data="fallen_help"),
     ],
 ]
 
