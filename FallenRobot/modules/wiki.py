0df3680e15eaf3d10244ff1ea50bd99da681598e
˹ᴧɴσɴʏᴍσᴜs ꭙ˼ <TheAnonymousX1025@gmail.com>
2023-06-04 16:02:46 +0530

    Update wiki.py
    Signed-off-by: ˹ᴧɴσɴʏᴍσᴜs ꭙ˼ <TheAnonymousX1025@gmail.com>

--- FallenRobot/modules/wiki.py
+++ FallenRobot/modules/wiki.py
@@ -57,6 +57,6 @@ WIKI_HANDLER = DisableAbleCommandHandler("wiki", wiki, run_async=True)
 dispatcher.add_handler(WIKI_HANDLER)
 
 __help__ = """
-» /wiki <ᴛᴇxᴛ> *:* sᴇᴀʀᴄʜs ᴀʙᴏᴜᴛ ᴛʜᴇ ɢɪᴠᴇɴ ᴛᴇxᴛ ᴏɴ ᴡɪᴋɪᴘᴇᴅɪᴀ.
+» /wiki (text) *:* Searchs about the given text on wikipedia.
 """
-__mod_name__ = "Wɪᴋɪ​"
+__mod_name__ = "Wɪᴋɪ"
