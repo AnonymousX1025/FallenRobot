d60210b867779079ce48344848791ea4cc229be5
AnonymousX1025 <TheAnonymousX1025@gmail.com>
2023-06-04 09:46:09 +0000

    Auto Fixes

--- FallenRobot/modules/chatbot.py
+++ FallenRobot/modules/chatbot.py
@@ -129,7 +129,9 @@ def chatbot(update: Update, context: CallbackContext):
         if not fallen_message(context, message):
             return
         bot.send_chat_action(chat_id, action="typing")
-        request = requests.get(f"https://kora-api.vercel.app/chatbot/2d94e37d-937f-4d28-9196-bd5552cac68b/{BOT_NAME}/Anonymous/message={message.text}")
+        request = requests.get(
+            f"https://kora-api.vercel.app/chatbot/2d94e37d-937f-4d28-9196-bd5552cac68b/{BOT_NAME}/Anonymous/message={message.text}"
+        )
         results = json.loads(request.text)
         sleep(0.5)
         message.reply_text(results["reply"])
