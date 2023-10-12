fd439c3481288a941b79e6f090095ea7e7f8c225
AnonymousX1025 <TheAnonymousX1025@gmail.com>
2023-10-12 12:27:16 +0000

    Auto Fixes

--- FallenRobot/modules/token.py
+++ FallenRobot/modules/token.py
@@ -4,7 +4,7 @@ from pyrogram import filters
 from pyrogram.types import Message
 
 from FallenRobot import pbot
-from FallenRobot.utils.mongo import is_user, add_user, remove_user
+from FallenRobot.utils.mongo import add_user, is_user, remove_user
 
 
 @pbot.on_message(filters.command("token") & filters.private)
@@ -19,15 +19,24 @@ async def gen_token(_, message: Message):
     api = f"{message.from_user.id}-fallen-{gen5}{gen1}{gen4}{gen2}{gen3}{gen3}{gen2}{gen4}{gen1}{gen5}"
     if not await is_user(message.from_user.id):
         await add_user(message.from_user.id, api)
-        await message.reply_text(f"ʜᴇʀᴇ ɪs ʏᴏᴜʀ ᴛᴏᴋᴇɴ ғᴏʀ ғᴀʟʟᴇɴ ᴄʜᴀᴛʙᴏᴛ ᴀᴘɪ :\n<code>{api}</code>\n\n<b>ᴅᴏɴ'ᴛ sʜᴀʀᴇ ᴛʜɪs ᴛᴏᴋᴇɴ ᴡɪᴛʜ ᴀɴʏᴏɴᴇ ᴇʟsᴇ.</b>")
+        await message.reply_text(
+            f"ʜᴇʀᴇ ɪs ʏᴏᴜʀ ᴛᴏᴋᴇɴ ғᴏʀ ғᴀʟʟᴇɴ ᴄʜᴀᴛʙᴏᴛ ᴀᴘɪ :\n<code>{api}</code>\n\n<b>ᴅᴏɴ'ᴛ sʜᴀʀᴇ ᴛʜɪs ᴛᴏᴋᴇɴ ᴡɪᴛʜ ᴀɴʏᴏɴᴇ ᴇʟsᴇ.</b>"
+        )
     else:
         fallen = await is_user(message.from_user.id)
-        await message.reply_text(f"ʏᴏᴜ'ᴠᴇ ᴀʟʀᴇᴀᴅʏ ɢᴇɴᴇʀᴀᴛᴇᴅ ʏᴏᴜʀ ᴛᴏᴋᴇɴ :\n<code>{fallen}</code>\n\n<b>ᴅᴏɴ'ᴛ sʜᴀʀᴇ ᴛʜɪs ᴛᴏᴋᴇɴ ᴡɪᴛʜ ᴀɴʏᴏɴᴇ ᴇʟsᴇ.</b>")
+        await message.reply_text(
+            f"ʏᴏᴜ'ᴠᴇ ᴀʟʀᴇᴀᴅʏ ɢᴇɴᴇʀᴀᴛᴇᴅ ʏᴏᴜʀ ᴛᴏᴋᴇɴ :\n<code>{fallen}</code>\n\n<b>ᴅᴏɴ'ᴛ sʜᴀʀᴇ ᴛʜɪs ᴛᴏᴋᴇɴ ᴡɪᴛʜ ᴀɴʏᴏɴᴇ ᴇʟsᴇ.</b>"
+        )
+
 
 @pbot.on_message(filters.command("revoke") & filters.private)
 async def rev_token(_, message: Message):
     if not await is_user(message.from_user.id):
-        return await message.reply_text(f"ʟᴏᴏᴋs ʟɪᴋᴇ ʏᴏᴜ'ᴠᴇ ɴᴏᴛ ɢᴇɴᴇʀᴀᴛᴇᴅ ᴛᴏᴋᴇɴ ʙᴇғᴏʀᴇ.")
+        return await message.reply_text(
+            f"ʟᴏᴏᴋs ʟɪᴋᴇ ʏᴏᴜ'ᴠᴇ ɴᴏᴛ ɢᴇɴᴇʀᴀᴛᴇᴅ ᴛᴏᴋᴇɴ ʙᴇғᴏʀᴇ."
+        )
     fallen = await is_user(message.from_user.id)
     await remove_user(message.from_user.id, fallen)
-    await message.reply_text(f"<b>ғᴀʟʟᴇɴ ᴄʜᴀᴛʙᴏᴛ ᴀᴘɪ ᴛᴏᴋᴇɴ ʀᴇᴠᴏᴋᴇᴅ sᴜᴄᴄᴇssғᴜʟʟʏ.</b>\n\nʏᴏᴜ ᴄᴀɴ ɢᴇɴᴇʀᴀᴛᴇ ʏᴏᴜʀ ᴛᴏᴋᴇɴ ᴀɢᴀɪɴ ʙʏ /token")
+    await message.reply_text(
+        f"<b>ғᴀʟʟᴇɴ ᴄʜᴀᴛʙᴏᴛ ᴀᴘɪ ᴛᴏᴋᴇɴ ʀᴇᴠᴏᴋᴇᴅ sᴜᴄᴄᴇssғᴜʟʟʏ.</b>\n\nʏᴏᴜ ᴄᴀɴ ɢᴇɴᴇʀᴀᴛᴇ ʏᴏᴜʀ ᴛᴏᴋᴇɴ ᴀɢᴀɪɴ ʙʏ /token"
+    )
