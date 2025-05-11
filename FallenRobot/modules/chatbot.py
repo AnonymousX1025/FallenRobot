736695deac07d5a57893e7abbae5ec64436ad245
˹ᴧɴσɴʏᴍσᴜs ꭙ˼ <notanony@hotmail.com>
2025-05-11 18:43:36 +0530

    Fix: chatbot api change
    Signed-off-by: ˹ᴧɴσɴʏᴍσᴜs ꭙ˼ <notanony@hotmail.com>

--- FallenRobot/modules/chatbot.py
+++ FallenRobot/modules/chatbot.py
@@ -130,7 +130,7 @@ def chatbot(update: Update, context: CallbackContext):
             return
         bot.send_chat_action(chat_id, action="typing")
         request = requests.get(
-            f"https://kora-api.vercel.app/chatbot/2d94e37d-937f-4d28-9196-bd5552cac68b/{BOT_NAME}/Anonymous/message={message.text}"
+            f"https://fallenxbot.vercel.app/chatbot/message={message.text}"
         )
         results = json.loads(request.text)
         sleep(0.5)
